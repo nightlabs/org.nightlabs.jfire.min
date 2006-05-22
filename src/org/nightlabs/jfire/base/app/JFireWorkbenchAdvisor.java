6589218087054ab62c3da0dfdf6f85949ceefedd
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-05-22 21:05:08 +0000

    - added creation of Seats for unnumbered sections - extended ip1-query GetHallList - continued work on Ip1-Ip2-Bridge: Import of VenueLayouts (fetching ip1-splans)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base@3931 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/app/JFireWorkbenchAdvisor.java
+++ src/org/nightlabs/jfire/base/app/JFireWorkbenchAdvisor.java
@@ -85,12 +85,11 @@ extends AbstractWorkbenchAdvisor
 			JFireRCDLDelegate.createSharedInstance(Login.getLogin(false), new File(JFireApplication.getRootDir(), "classloader.cache"));
 		} catch (LoginException e) {
 			throw e;
+		} catch (IOException e) {
+			LoginException x = new LoginException(e.getMessage());
+			x.initCause(e);
+			throw x;
 		}
-//		catch (IOException e) {
-//			LoginException x = new LoginException(e.getMessage());
-//			x.initCause(e);
-//			throw x;
-//		}
 		initializeLoginModule();
 	}
 	   	    
