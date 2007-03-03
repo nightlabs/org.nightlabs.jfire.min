0425efb82782e50ddc58166ab712dc1e48ac312a
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-03-03 16:42:08 +0000

    added constructor
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireServerManager@5559 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/init/InitException.java
+++ src/org/nightlabs/jfire/init/InitException.java
@@ -1,10 +1,14 @@
 package org.nightlabs.jfire.init;
 
 public class InitException extends Exception {
+	public InitException(Exception cause) {
+		super(cause);
+	}
+
 	public InitException(String message, Exception cause) {
 		super(message, cause);
 	}
-	
+
 	public InitException(String message) {
 		super(message);
 	}
