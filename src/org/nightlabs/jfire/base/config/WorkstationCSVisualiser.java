bdcee33534c1d4a6425182d112850830987dc5b9
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-05-04 08:38:53 +0000

    reverted BCEL error bla
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base@6401 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/config/WorkstationCSVisualiser.java
+++ src/org/nightlabs/jfire/base/config/WorkstationCSVisualiser.java
@@ -28,8 +28,9 @@ package org.nightlabs.jfire.base.config;
 
 import javax.jdo.FetchPlan;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.nightlabs.jdo.NLJDOHelper;
-import org.nightlabs.jfire.base.workstation.WorkstationProvider;
+import org.nightlabs.jfire.base.workstation.WorkstationDAO;
 import org.nightlabs.jfire.config.ConfigGroup;
 import org.nightlabs.jfire.config.ConfigSetup;
 import org.nightlabs.jfire.config.id.ConfigID;
@@ -57,7 +58,11 @@ public class WorkstationCSVisualiser implements ConfigSetupVisualiser {
 	public String getKeyObjectName(ConfigID configID) {
 		try {
 			WorkstationID workstaionID = new WorkstationID(configID.configKey);
-			Workstation workstation = WorkstationProvider.sharedInstance().getWorkstation(workstaionID, WORKSTATION_FETCH_GROUPS, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+			Workstation workstation = WorkstationDAO.sharedInstance().getWorkstation(
+					workstaionID, WORKSTATION_FETCH_GROUPS, 
+					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, 
+					new NullProgressMonitor()
+				);
 			return workstation.getWorkstationID();
 		} catch (Exception e) {
 			return configID.configKey;
