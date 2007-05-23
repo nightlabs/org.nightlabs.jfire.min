ca2fb89b20704adbec92b7ac3147a6f1cf89f3b7
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-05-23 17:25:21 +0000

    added TaskDAO to server project
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseDAO@6770 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/timer/dao/TaskDAO.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.timer.dao;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import javax.jdo.FetchPlan;
+
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.timer.Task;
+import org.nightlabs.jfire.timer.TimerManager;
+import org.nightlabs.jfire.timer.TimerManagerUtil;
+import org.nightlabs.jfire.timer.id.TaskID;
+import org.nightlabs.progress.ProgressMonitor;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class TaskDAO extends BaseJDOObjectDAO<TaskID, Task> {
+
+	
+	public static final String[] DEFAULT_FETCH_GROUP = { FetchPlan.DEFAULT,
+		Task.FETCH_GROUP_NAME, Task.FETCH_GROUP_DESCRIPTION,
+		Task.FETCH_GROUP_USER 
+	};
+	
+	/**
+	 * 
+	 */
+	public TaskDAO() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.base.jdo.JDOObjectDAO#retrieveJDOObjects(java.util.Set, java.lang.String[], int, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	protected Collection<Task> retrieveJDOObjects(Set<TaskID> objectIDs, String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor) 
+	throws Exception 
+	{
+		TimerManager timerManager = TimerManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
+		return timerManager.getTasks(objectIDs, fetchGroups, maxFetchDepth);
+	}
+	
+	public Task getTask(
+			TaskID taskID, String[] fetchGroups, ProgressMonitor monitor
+		) 
+	{
+		return getJDOObject(null, taskID, fetchGroups, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor);
+	}
+	
+	public List<Task> getTasks(
+			Collection<TaskID> taskIDs, 
+			String[] fetchGroups, ProgressMonitor monitor
+		) 
+	{
+		return getJDOObjects(null, taskIDs, fetchGroups, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor);
+	}
+	
+	private static TaskDAO sharedInstance;
+	
+	public static TaskDAO sharedInstance() {
+		if (sharedInstance == null)
+			sharedInstance = new TaskDAO();
+		return sharedInstance;
+	}
+
+}
