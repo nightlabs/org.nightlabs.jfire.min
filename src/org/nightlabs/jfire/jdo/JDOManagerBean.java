a0f0caa6d542b10f7db3eccc52630cb3c9b656cb
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-06-19 18:51:15 +0000

    - bugfix: https://www.jfire.org/modules/bugs/view.php?id=69 - uncommented some old (unnecessary?!) code in org.nightlabs.jfire.scripting - made deployment in JBossLibs verbose
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseBean@7360 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jdo/JDOManagerBean.java
+++ src/org/nightlabs/jfire/jdo/JDOManagerBean.java
@@ -29,7 +29,9 @@ package org.nightlabs.jfire.jdo;
 import java.rmi.RemoteException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
 
 import javax.ejb.CreateException;
 import javax.ejb.EJBException;
@@ -39,14 +41,11 @@ import javax.jdo.PersistenceManager;
 
 import org.apache.log4j.Logger;
 import org.nightlabs.ModuleException;
-import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.jdo.ObjectIDUtil;
 import org.nightlabs.jfire.base.BaseSessionBeanImpl;
 import org.nightlabs.jfire.jdo.cache.CacheManager;
 import org.nightlabs.jfire.jdo.cache.NotificationBundle;
-import org.nightlabs.jfire.jdo.controller.JDOObjectChangeEvent;
-import org.nightlabs.jfire.jdo.controller.JDOObjectController;
-import org.nightlabs.jfire.jdo.controller.JDOObjectSyncResult;
+import org.nightlabs.jfire.jdo.notification.DirtyObjectID;
 import org.nightlabs.jfire.jdo.notification.IJDOLifecycleListenerFilter;
 import org.nightlabs.jfire.jdo.organisationsync.DirtyObjectIDCarrier;
 import org.nightlabs.jfire.jdo.organisationsync.IncomingChangeListenerDescriptor;
@@ -105,17 +104,17 @@ implements SessionBean
 	{
 		try {
 			Class clazz;
-	
+
 			CacheManager cm = getLookup().getCacheManager();
 			try {
 				clazz = cm.getClassByObjectID(objectID, false);
 			} finally {
 				cm.close();
 			}
-	
+
 			if (clazz != null)
 				return clazz.getName();
-	
+
 			PersistenceManager pm = getPersistenceManager();
 			try {
 				Object o = pm.getObjectById(objectID);
@@ -140,19 +139,18 @@ implements SessionBean
 	 * The remove action will be performed before the add action. Hence, if an objectID
 	 * is in both of them, it will be added in total. 
 	 *
-	 * @param cacheSessionID The ID of your session.
 	 * @param removeObjectIDs Either <tt>null</tt> or the object-ids of those JDO objects for which to remove listeners
 	 * @param addObjectIDs Either <tt>null</tt> or the object-ids of those JDO objects for which to add listeners
 	 *
 	 * @ejb.interface-method
 	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Supports"
+	 * @ejb.transaction type="Supports"
 	 */
-	public void removeAddListeners(
+	public void removeOrAddListeners(
 			Collection<Object> removeObjectIDs,
 			Collection<Object> addObjectIDs,
-			Collection<IJDOLifecycleListenerFilter> addFilters,
-			Collection<Long> removeFilterIDs)
+			Collection<Long> removeFilterIDs,
+			Collection<IJDOLifecycleListenerFilter> addFilters)
 	throws ModuleException
 	{
 		CacheManager cm = getLookup().getCacheManager(getPrincipal());
@@ -164,11 +162,37 @@ implements SessionBean
 			if (addObjectIDs != null)
 				cm.addChangeListeners(addObjectIDs);
 
+			if (removeFilterIDs != null)
+				cm.removeLifecycleListenerFilters(removeFilterIDs);
+
 			if (addFilters != null)
 				cm.addLifecycleListenerFilters(addFilters);
 
-			if (removeFilterIDs != null)
-				cm.removeLifecycleListenerFilters(removeFilterIDs);
+			if (logger.isDebugEnabled()) {
+				logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"): removeObjectIDs.size()=" + (removeObjectIDs == null ? null : removeObjectIDs.size()));
+				if (removeObjectIDs != null) {
+					for (Object oid : removeObjectIDs)
+						logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"):   * " + oid);
+				}
+
+				logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"): addObjectIDs.size()=" + (addObjectIDs == null ? null : addObjectIDs.size()));
+				if (addObjectIDs != null) {
+					for (Object oid : addObjectIDs)
+						logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"):   * " + oid);
+				}
+
+				logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"): removeFilterIDs.size()=" + (removeFilterIDs == null ? null : removeFilterIDs.size()));
+				if (removeFilterIDs != null) {
+					for (Long filterID : removeFilterIDs)
+						logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"):   * " + filterID);
+				}
+
+				logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"): addFilters.size()=" + (addFilters == null ? null : addFilters.size()));
+				if (addFilters != null) {
+					for (IJDOLifecycleListenerFilter filter : addFilters)
+						logger.debug("removeOrAddListeners(sessionID="+getSessionID()+"):   * " + filter.getFilterID().getFilterID());
+				}
+			}
 
 		} finally {
 			cm.close();
@@ -198,42 +222,56 @@ implements SessionBean
 		CacheManager cm = getLookup().getCacheManager(getPrincipal());
 		try {
 			cm.resubscribeAllListeners(subscribedObjectIDs, filters);
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("resubscribeAllListeners(sessionID="+getSessionID()+"): subscribedObjectIDs.size()=" + (subscribedObjectIDs == null ? null : subscribedObjectIDs.size()));
+				if (subscribedObjectIDs != null) {
+					for (Object oid : subscribedObjectIDs)
+						logger.debug("resubscribeAllListeners(sessionID="+getSessionID()+"):   * " + oid);
+				}
+
+				logger.debug("resubscribeAllListeners(sessionID="+getSessionID()+"): filters.size()=" + (filters == null ? null : filters.size()));
+				if (filters != null) {
+					for (IJDOLifecycleListenerFilter filter : filters)
+						logger.debug("resubscribeAllListeners(sessionID="+getSessionID()+"):   * " + filter.getFilterID().getFilterID());
+				}
+			}
 		} finally {
 			cm.close();
 		}
 	}
 
-	/**
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Supports"
-	 */
-	public void addLifecycleListenerFilters(Collection<IJDOLifecycleListenerFilter> filters)
-	throws ModuleException
-	{
-		CacheManager cm = getLookup().getCacheManager(getPrincipal());
-		try {
-			cm.addLifecycleListenerFilters(filters);
-		} finally {
-			cm.close();
-		}
-	}
-
-	/**
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Supports"
-	 */
-	public void removeLifecycleListenerFilters(Set<Long> filterIDs)
-	throws ModuleException
-	{
-		CacheManager cm = getLookup().getCacheManager(getPrincipal());
-		try {
-			cm.removeLifecycleListenerFilters(filterIDs);
-		} finally {
-			cm.close();
-		}
-	}
+//	/**
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Supports"
+//	 */
+//	public void addLifecycleListenerFilters(Collection<IJDOLifecycleListenerFilter> filters)
+//	throws ModuleException
+//	{
+//		CacheManager cm = getLookup().getCacheManager(getPrincipal());
+//		try {
+//			cm.addLifecycleListenerFilters(filters);
+//		} finally {
+//			cm.close();
+//		}
+//	}
+//
+//	/**
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Supports"
+//	 */
+//	public void removeLifecycleListenerFilters(Set<Long> filterIDs)
+//	throws ModuleException
+//	{
+//		CacheManager cm = getLookup().getCacheManager(getPrincipal());
+//		try {
+//			cm.removeLifecycleListenerFilters(filterIDs);
+//		} finally {
+//			cm.close();
+//		}
+//	}
 
 	/**
 	 * This method removes all listeners that have been registered for
@@ -282,7 +320,30 @@ implements SessionBean
 	{
 		CacheManager cm = getLookup().getCacheManager(getPrincipal());
 		try {
-			return cm.waitForChanges(waitTimeout);
+			NotificationBundle notificationBundle = cm.waitForChanges(waitTimeout);
+			if (logger.isDebugEnabled()) {
+				if (notificationBundle == null)
+					logger.debug("waitForChanges(sessionID="+getSessionID()+"): notificationBundle is null.");
+				else {
+					logger.debug("waitForChanges(sessionID="+getSessionID()+"): notificationBundle.getDirtyObjectIDs().size()=" + (notificationBundle.getDirtyObjectIDs() == null ? null : notificationBundle.getDirtyObjectIDs().size()));
+					if (notificationBundle.getDirtyObjectIDs() != null) {
+						for (DirtyObjectID dirtyObjectID : notificationBundle.getDirtyObjectIDs())
+							logger.debug("waitForChanges(sessionID="+getSessionID()+"):   * " + dirtyObjectID);
+					}
+
+					logger.debug("waitForChanges(sessionID="+getSessionID()+"): notificationBundle.getFilterID2dirtyObjectIDs().size()=" + (notificationBundle.getFilterID2dirtyObjectIDs() == null ? null : notificationBundle.getFilterID2dirtyObjectIDs().size()));
+					if (notificationBundle.getFilterID2dirtyObjectIDs() != null) {
+						for (Map.Entry<Long, SortedSet<DirtyObjectID>> me1 : notificationBundle.getFilterID2dirtyObjectIDs().entrySet()) {
+							logger.debug("waitForChanges(sessionID="+getSessionID()+"):   * filterID=" + me1.getKey() + " dirtyObjectIDs.size()=" + (me1.getValue() == null ? null : me1.getValue().size()));
+							if (me1.getValue() != null) {
+								for (DirtyObjectID dirtyObjectID : me1.getValue())
+									logger.debug("waitForChanges(sessionID="+getSessionID()+"):     - " + dirtyObjectID);
+							}
+						}
+					}
+				}
+			}
+			return notificationBundle;
 		} finally {
 			cm.close();
 		}
@@ -345,85 +406,85 @@ implements SessionBean
 		} 
 	}
 	
-	/**
-	 *
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Required"
-	 */
-	public JDOObjectController getJDOObjectController(ObjectID linkObject, String[] fetchGroups, int maxFetchDepth)
-	throws ModuleException
-	{
-		return getJDOObjectController(linkObject.toString(), fetchGroups, maxFetchDepth);
-	}
-	
-	/**
-	 *
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Required"
-	 */
-	public JDOObjectController getJDOObjectController(String linkObject, String[] fetchGroups, int maxFetchDepth)
-	throws ModuleException
-	{
-		PersistenceManager pm = getPersistenceManager();
-		try {
-			pm.getFetchPlan().setMaxFetchDepth(maxFetchDepth);
-
-			if (fetchGroups != null)
-				pm.getFetchPlan().setGroups(fetchGroups);
-			else
-				pm.getFetchPlan().setGroups(JDOObjectController.DEFAULT_FETCH_GROUPS);
-			
-			JDOObjectController controller = JDOObjectController.getObjectController(pm, linkObject);
-			
-			return (JDOObjectController) pm.detachCopy(controller);
-			
-		} finally {
-			pm.close();
-		}
-		
-	}
-	
-	/**
-	 *
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Required"
-	 */
-	public JDOObjectSyncResult syncJDOObjectChanges(String linkObject, long version, String[] fetchGroups, int maxFetchDepth)
-	throws ModuleException
-	{
-		PersistenceManager pm = getPersistenceManager();
-		try {
-			pm.getFetchPlan().setMaxFetchDepth(maxFetchDepth);
-
-			if (fetchGroups == null)
-				pm.getFetchPlan().setGroups(JDOObjectSyncResult.DEFAULT_FETCH_GROUPS);
-			else
-				pm.getFetchPlan().setGroups(fetchGroups);
-			
-			JDOObjectController controller = JDOObjectController.getObjectController(pm, linkObject);
-			JDOObjectController dController = (JDOObjectController)pm.detachCopy(controller);
-			
-			Collection<JDOObjectChangeEvent> events = JDOObjectChangeEvent.getChangeEventsAfterVersion(pm, linkObject, version);
-			Collection dEvents = pm.detachCopyAll(events);			
-			return new JDOObjectSyncResult(dController, dEvents);
-		} finally {
-			pm.close();
-		}
-		
-	}	
-	
-	/**
-	 *
-	 * @ejb.interface-method
-	 * @ejb.permission role-name="_Guest_"
-	 * @ejb.transaction type = "Required"
-	 */
-	public JDOObjectSyncResult syncJDOObjectChanges(ObjectID linkObject, long version, String[] fetchGroups, int maxFetchDepth)
-	throws ModuleException
-	{
-		return syncJDOObjectChanges(linkObject.toString(), version, fetchGroups, maxFetchDepth);
-	}	
+//	/**
+//	 *
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Required"
+//	 */
+//	public JDOObjectController getJDOObjectController(ObjectID linkObject, String[] fetchGroups, int maxFetchDepth)
+//	throws ModuleException
+//	{
+//		return getJDOObjectController(linkObject.toString(), fetchGroups, maxFetchDepth);
+//	}
+//	
+//	/**
+//	 *
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Required"
+//	 */
+//	public JDOObjectController getJDOObjectController(String linkObject, String[] fetchGroups, int maxFetchDepth)
+//	throws ModuleException
+//	{
+//		PersistenceManager pm = getPersistenceManager();
+//		try {
+//			pm.getFetchPlan().setMaxFetchDepth(maxFetchDepth);
+//
+//			if (fetchGroups != null)
+//				pm.getFetchPlan().setGroups(fetchGroups);
+//			else
+//				pm.getFetchPlan().setGroups(JDOObjectController.DEFAULT_FETCH_GROUPS);
+//			
+//			JDOObjectController controller = JDOObjectController.getObjectController(pm, linkObject);
+//			
+//			return (JDOObjectController) pm.detachCopy(controller);
+//			
+//		} finally {
+//			pm.close();
+//		}
+//		
+//	}
+//	
+//	/**
+//	 *
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Required"
+//	 */
+//	public JDOObjectSyncResult syncJDOObjectChanges(String linkObject, long version, String[] fetchGroups, int maxFetchDepth)
+//	throws ModuleException
+//	{
+//		PersistenceManager pm = getPersistenceManager();
+//		try {
+//			pm.getFetchPlan().setMaxFetchDepth(maxFetchDepth);
+//
+//			if (fetchGroups == null)
+//				pm.getFetchPlan().setGroups(JDOObjectSyncResult.DEFAULT_FETCH_GROUPS);
+//			else
+//				pm.getFetchPlan().setGroups(fetchGroups);
+//			
+//			JDOObjectController controller = JDOObjectController.getObjectController(pm, linkObject);
+//			JDOObjectController dController = (JDOObjectController)pm.detachCopy(controller);
+//			
+//			Collection<JDOObjectChangeEvent> events = JDOObjectChangeEvent.getChangeEventsAfterVersion(pm, linkObject, version);
+//			Collection dEvents = pm.detachCopyAll(events);			
+//			return new JDOObjectSyncResult(dController, dEvents);
+//		} finally {
+//			pm.close();
+//		}
+//		
+//	}	
+//	
+//	/**
+//	 *
+//	 * @ejb.interface-method
+//	 * @ejb.permission role-name="_Guest_"
+//	 * @ejb.transaction type = "Required"
+//	 */
+//	public JDOObjectSyncResult syncJDOObjectChanges(ObjectID linkObject, long version, String[] fetchGroups, int maxFetchDepth)
+//	throws ModuleException
+//	{
+//		return syncJDOObjectChanges(linkObject.toString(), version, fetchGroups, maxFetchDepth);
+//	}
 }
