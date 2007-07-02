dd357c77404a12e1e339854ad8cd452e09109f7c
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-07-02 18:13:13 +0000

    AbstractJDOSelectionZoneActionHandler
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base@7585 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/base/search/AbstractJDOSelectionZoneActionHandler.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.base.search;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.nightlabs.base.notification.SelectionManager;
+import org.nightlabs.base.search.AbstractSelectionZoneActionHandler;
+import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.notification.NotificationEvent;
+
+/**
+ * @author Daniel Mazurek - daniel <at> nightlabs <dot> de
+ *
+ */
+public abstract class AbstractJDOSelectionZoneActionHandler 
+extends AbstractSelectionZoneActionHandler 
+{
+	public void run() {
+		Collection selectedObjects = getSearchResultProvider().getSelectedObjects();
+		Collection selectedObjectIDs = NLJDOHelper.getObjectIDSet(selectedObjects);
+		Collection<Class> subjectClassesToClear = new ArrayList<Class>();
+		subjectClassesToClear.add(getSearchResultProvider().getFactory().getResultTypeClass());
+		if (selectedObjects != null) {
+			SelectionManager.sharedInstance().notify(new NotificationEvent(
+					AbstractJDOSelectionZoneActionHandler.this, 
+					getSelectionZone(), 
+					selectedObjectIDs, 
+					subjectClassesToClear));
+		}
+	}
+
+}
