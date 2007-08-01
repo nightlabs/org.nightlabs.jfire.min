4cecee036ef3bbb0d2f358eb8bfc701fbdb29ec7
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-08-01 19:11:38 +0000

    minor changes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@7942 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/cascadedauthentication/CascadedAuthenticationTestSuite.java
+++ src/org/nightlabs/jfire/testsuite/cascadedauthentication/CascadedAuthenticationTestSuite.java
@@ -1,9 +1,17 @@
 package org.nightlabs.jfire.testsuite.cascadedauthentication;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.jdo.PersistenceManager;
+import javax.naming.InitialContext;
 
 import junit.framework.TestCase;
 
+import org.nightlabs.jfire.servermanager.JFireServerManager;
+import org.nightlabs.jfire.servermanager.JFireServerManagerFactory;
+import org.nightlabs.jfire.servermanager.config.OrganisationCf;
 import org.nightlabs.jfire.testsuite.TestSuite;
 
 public class CascadedAuthenticationTestSuite
@@ -15,18 +23,53 @@ extends TestSuite
 	public CascadedAuthenticationTestSuite(Class<? extends TestCase>... classes) {
 		super(classes);
 		setName("Test JFire Cascaded Authentication (cross organisation communication)");
+		requiredOrganisationIDs.add("jfire.nightlabs.org"); // TODO fetch from config
+		requiredOrganisationIDs.add("chezfrancois.jfire.org");
+		requiredOrganisationIDs.add("reseller.jfire.org");
 	}
+
+	private Set<String> requiredOrganisationIDs = new HashSet<String>();
 
 	@Override
 	public String canRunTests(PersistenceManager pm)
 	throws Exception
 	{
-		String className = "org.nightlabs.jfire.security.User";
+		// as a first quick-check, we search for the module JFireDemoSetupMultiOrganisation
+		String className = "org.nightlabs.demo.multiorg.MultiOrganisationServerInitialiser";
 		try {
 			Class.forName(className);
 		} catch (ClassNotFoundException x) {			
-			return "The module JFireBase seems not to be installed (Class \"" + className + "\" could not be found)!";
+			return "The module JFireDemoSetupMultiOrganisation seems not to be installed (Class \"" + className + "\" could not be found)!";
 		}
+
+		// then we check whether the required organisations all exist and know each other.
+		JFireServerManager jfsm = null;
+		InitialContext initialContext = new InitialContext();
+		try {
+			JFireServerManagerFactory jfsmf = (JFireServerManagerFactory) initialContext.lookup(JFireServerManagerFactory.JNDI_NAME);
+			jfsm = jfsmf.getJFireServerManager();
+
+			// which organisations exist on the local server?
+			List<OrganisationCf> organisationCfs = jfsm.getOrganisationCfs(false);
+			Set<String> organisationIDs = new HashSet<String>(organisationCfs.size());
+			for (OrganisationCf organisationCf : organisationCfs)
+				organisationIDs.add(organisationCf.getOrganisationID());
+
+			// are all required organisations here?
+			for (String requiredOrganisationID : requiredOrganisationIDs) {
+				if (!organisationIDs.contains(requiredOrganisationID))
+					return "This server does not host the demo-organisation \"" + requiredOrganisationID + "\" which should have been created by the module JFireDemoSetupMultiOrganisation.";
+			}
+
+			// TODO are the cross-organisation-registrations complete?
+
+		} finally {
+			initialContext.close();
+
+			if (jfsm != null)
+				jfsm.close();
+		}
+
 		return null;
 	}
 
--- src/org/nightlabs/jfire/testsuite/login/LoginTest.java
+++ src/org/nightlabs/jfire/testsuite/login/LoginTest.java
@@ -23,14 +23,14 @@ public class LoginTest extends TestCase
 
 	public void testCorrectLogin() throws Exception {
 		JFireLogin login = JFireTestLogin.getUserLogin(JFireTestLogin.USER_QUALIFIER_SERVER_ADMIN);
-		login.login();
+		login.login(); // Warning! even though it's fine for testing here, switching the identity during a running transaction might cause trouble.
 		UserManagerUtil.getHome(login.getInitialContextProperties()).create();		
 	}
 
 	public void testInCorrectLogin() {
 		JFireLogin login = new JFireLogin("chezfrancois.jfire.org", "francoiz", "text");
 		try {
-			login.login();
+			login.login(); // Warning! even though it's fine for testing here, switching the identity during a running transaction might cause trouble.
 		} catch (LoginException e) {
 			// OK, caught some login exception
 			return;
