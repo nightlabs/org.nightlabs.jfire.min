094fcbf574e2f2fc33ee748a6ed565b353b7aff6
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-08-10 17:25:23 +0000

    - Refactored PropertySet field and method names - PIM can edit and change Contacts
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseDAO@8034 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/security/dao/UserDAO.java
+++ src/org/nightlabs/jfire/security/dao/UserDAO.java
@@ -144,8 +144,8 @@ public class UserDAO extends BaseJDOObjectDAO<UserID, User>
 			Person person = user.getPerson();
 			IStruct struct = person.getStructure(); // StructLocalDAO.sharedInstance().getStructLocal(Person.class.getName(), StructLocal.DEFAULT_SCOPE);
 			if (user.getPerson() != null)
-				struct.implodeProperty(user.getPerson());
-			long activePersonID = person.getPropertyID();
+				struct.implodePropertySet(user.getPerson());
+			long activePersonID = person.getPropertySetID();
 			if (activePersonID == PropertySet.TEMPORARY_PROP_ID) {
 				person.assignID(IDGenerator.nextID(PropertySet.class));
 				// FIXME: how to do this?
@@ -176,7 +176,7 @@ public class UserDAO extends BaseJDOObjectDAO<UserID, User>
 			User result = um.storeUser(user, userPassword, get, fetchGroups, maxFetchDepth);
 			monitor.worked(1);
 			person = result.getPerson();
-			struct.explodeProperty(person);    
+			struct.explodePropertySet(person);
 			monitor.worked(1);
 			monitor.done();
 			return result;
