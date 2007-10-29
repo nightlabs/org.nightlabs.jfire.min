5d6b9f4edf003ab3ac6b11c895df168675872e24
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-10-29 19:07:34 +0000

    - refactored rcp login mechanism to use LoginData instead of JFireLoginContext.  -> also refactored all classes involved in this mechanism like JFireLoginAuthCallbackHanlder, etc.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseClientLib@9121 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/login/JFireLogin.java
+++ src/org/nightlabs/jfire/base/login/JFireLogin.java
@@ -36,6 +36,8 @@ import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 
 import org.apache.log4j.Logger;
+import org.nightlabs.j2ee.LoginData;
+import org.nightlabs.math.Base62Coder;
 
 /**
  * Helper class to login to a JFire server.
@@ -61,6 +63,7 @@ import org.apache.log4j.Logger;
  * </p> * 
  * 
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ * @author Marius Heinzmann -- Marius[at]NightLabs[dot]de
  */
 public class JFireLogin
 {
@@ -69,51 +72,51 @@ public class JFireLogin
 	 */
 	private static final Logger logger = Logger.getLogger(JFireLogin.class);
 	
-	public static final String PROP_ORGANISATION_ID = "jfire.login.organisationID";
-	public static final String PROP_USER_ID = "jfire.login.userID";
-	public static final String PROP_PASSWORD = "jfire.login.password";
-	public static final String PROP_PROVIDER_URL = "jfire.login.providerURL";
-	public static final String PROP_INITIAL_CONTEXT_FACTORY = "jfire.login.initialContextFactory";
-	public static final String PROP_SECURITY_PROTOCOL = "jfire.login.securityProtocol";
+	public static final String LOGIN_PREFIX = "jfire.login.";
+	public static final String PROP_ORGANISATION_ID = LOGIN_PREFIX + "organisationID";
+	public static final String PROP_USER_ID = LOGIN_PREFIX + "userID";
+	public static final String PROP_PASSWORD = LOGIN_PREFIX + "password";
+	public static final String PROP_PROVIDER_URL = LOGIN_PREFIX + "providerURL";
+	public static final String PROP_INITIAL_CONTEXT_FACTORY = LOGIN_PREFIX + "initialContextFactory";
+	public static final String PROP_SECURITY_PROTOCOL = LOGIN_PREFIX + "securityProtocol";
+	public static final String PROP_WORKSTATION_ID = LOGIN_PREFIX + LoginData.WORKSTATION_ID;
 	
-	private String organisationID;
-	private String userID;
-	// username is the concatted product of userID and organisationID (userID@organisationID)
-	private String username;
-	private String password;
-	private String sessionID;
-	private String providerURL;
-	private String initialContextFactory;
-	private String securityProtocol;
+	/**
+	 * Encapsulates all necessary login information.
+	 */
+	private LoginData loginData;
 	
-//	private Properties loginProperties;
-
 	/**
 	 * Creates a new {@link JFireLogin}.
 	 * The values like username and password will 
 	 * be taken from the given Properties, 
 	 * see the class documentation for more details.
 	 * 
-	 * @param loginProperties The login configuration.
+	 * @param loginProperties The login configuration, this can also be a pr
 	 */
-	public JFireLogin(Properties loginProperties)
+	public JFireLogin(Properties properties)
 	{
-//		this.loginProperties = loginProperties;
-		this.organisationID = loginProperties.getProperty(PROP_ORGANISATION_ID, "");
-		this.userID = loginProperties.getProperty(PROP_USER_ID, "");
-		this.password = loginProperties.getProperty(PROP_PASSWORD, "");
-		this.providerURL = loginProperties.getProperty(PROP_PROVIDER_URL, null);
-		this.initialContextFactory = loginProperties.getProperty(PROP_INITIAL_CONTEXT_FACTORY, "org.nightlabs.jfire.jboss.cascadedauthentication.LoginInitialContextFactory");
-		this.securityProtocol = loginProperties.getProperty(PROP_SECURITY_PROTOCOL, "jfire");
-//		// TODO is this correct here or should we 
-//		this.sessionID = Base62Coder.sharedInstance().encode(System.currentTimeMillis(), 1) + '-' + Base62Coder.sharedInstance().encode((long)(10000 * Math.random()), 1);
-		this.sessionID = JFireLogin.class.getName(); // TODO how do we handle the sessionID?!?!
-
-		StringBuffer sbUser = new StringBuffer();
-		sbUser.append(userID);
-		sbUser.append('@');
-		sbUser.append(organisationID);
-		username = sbUser.toString();
+		Properties loginProps = org.nightlabs.util.Properties.getProperties(properties, LOGIN_PREFIX);
+		String organisationID, userID, password;
+		organisationID = loginProps.getProperty(PROP_ORGANISATION_ID, "");
+		userID = loginProps.getProperty(PROP_USER_ID, "");
+		password = loginProps.getProperty(PROP_PASSWORD, "");
+		loginData = new LoginData(organisationID, userID, password);
+		
+		for (Object propKey : loginProps.keySet()) {
+			if (PROP_PROVIDER_URL.equals(propKey))
+				loginData.setProviderURL(loginProps.getProperty(PROP_PROVIDER_URL, null));
+			else if (PROP_INITIAL_CONTEXT_FACTORY.equals(propKey))
+				loginData.setInitialContextFactory(loginProps.getProperty(PROP_INITIAL_CONTEXT_FACTORY, "org.nightlabs.jfire.jboss.cascadedauthentication.LoginInitialContextFactory"));
+			else if (PROP_SECURITY_PROTOCOL.equals(propKey))
+				loginData.setSecurityProtocol(loginProps.getProperty(PROP_SECURITY_PROTOCOL, "jfire"));
+			else {
+				loginData.getAdditionalParams().put(
+						((String)propKey).substring(LOGIN_PREFIX.length()), 
+						loginProps.getProperty((String)propKey)
+						);
+			}
+		}
 	}
 
 	/**
@@ -143,40 +146,38 @@ public class JFireLogin
 	 * @return The organisationID.
 	 */
 	public String getOrganisationID() {
-		return organisationID;
+		return loginData.getOrganisationID();
 	}
 	/**
 	 * @return The userID.
 	 */
 	public String getUserID() {
-		return userID;
+		return loginData.getOrganisationID();
 	}
 	/**
 	 * @return The password.
 	 */
 	public String getPassword() {
-		return password;
+		return loginData.getPassword();
 	}
 	
 	/**
 	 * @return The sessionID.
 	 */
 	public String getSessionID() {
-		return sessionID;
+		return loginData.getSessionID();
 	}
 	
 	// *** end of properties *************************
 	
 	protected transient Properties initialContextProperties = null;
-//	protected transient InitialContext initialContext = null;
-//	protected transient LoginContext loginContext = null;
 
 	/**
-	 * @return The {@link CallbackHandler} that can serve JFire login credentials accoring to this {@link JFireLogin}.
+	 * @return The {@link CallbackHandler} that can serve JFire login credentials according to this {@link JFireLogin}.
 	 */
 	public JFireLoginAuthCallbackHandler getAuthCallbackHandler()
 	{
-		return new JFireLoginAuthCallbackHandler(organisationID, userID, sessionID, password.toCharArray());
+		return new JFireLoginAuthCallbackHandler(loginData);
 	}
 	
 	/**
@@ -195,6 +196,7 @@ public class JFireLogin
 		if (initialContextProperties == null) {
 			logger.debug(this.getClass().getName()+"#getInitialContextProperties(): generating props");
 
+			String providerURL = loginData.getProviderURL();
 			if (providerURL == null) {
 				InitialContext initialContext = new InitialContext();
 				providerURL = (String) initialContext.getEnvironment().get(Context.PROVIDER_URL); // TODO does this really work? Alternatively, we might read it from a registry - in the server, this registry exists already (but this is a client lib)...
@@ -206,12 +208,11 @@ public class JFireLogin
 			
 
 			Properties props = new Properties();
-//			props.put(InitialContext.INITIAL_CONTEXT_FACTORY,	"org.jboss.security.jndi.LoginInitialContextFactory");
-			props.put(Context.INITIAL_CONTEXT_FACTORY, initialContextFactory);
+			props.put(Context.INITIAL_CONTEXT_FACTORY, loginData.getInitialContextFactory());
 			props.put(Context.PROVIDER_URL, providerURL);
-			props.put(Context.SECURITY_PRINCIPAL, username);
-			props.put(Context.SECURITY_CREDENTIALS, password);
-			props.put(Context.SECURITY_PROTOCOL, securityProtocol);
+			props.put(Context.SECURITY_PRINCIPAL, loginData.getPrincipalName());
+			props.put(Context.SECURITY_CREDENTIALS, loginData.getPassword());
+			props.put(Context.SECURITY_PROTOCOL, loginData.getSecurityProtocol());
 
 			initialContextProperties = props;
 		}
@@ -233,6 +234,11 @@ public class JFireLogin
 	public void login() 
 	throws LoginException 
 	{
+		Base62Coder coder = Base62Coder.sharedInstance();
+		loginData.setSessionID(
+				coder.encode(System.currentTimeMillis(), 1) + '-' +
+				coder.encode((long)(Math.random() * 14776335), 1)); // 14776335 is the highest value encoded in 4 digits ("zzzz")
+
 		loginContext = new LoginContext("jfire", getAuthCallbackHandler());
 		loginContext.login();
 	}
@@ -249,6 +255,7 @@ public class JFireLogin
 			loginContext.logout();
 			loginContext = null;
 			initialContextProperties = null;
+			loginData.setSessionID(null);
 		}
 	}
 	
--- src/org/nightlabs/jfire/base/login/JFireLoginAuthCallbackHandler.java
+++ src/org/nightlabs/jfire/base/login/JFireLoginAuthCallbackHandler.java
@@ -8,26 +8,27 @@ import javax.security.auth.callback.NameCallback;
 import javax.security.auth.callback.PasswordCallback;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
+import org.nightlabs.j2ee.LoginData;
+
 /**
  * CallBackHandler for {@link JFireLogin}.
  *  
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ * @author Marius Heinzmann -- Marius[at]NightLabs[dot]de
  */
 public class JFireLoginAuthCallbackHandler implements CallbackHandler
 {
-	private String organisationID;
-	private String userID;
-	private String sessionID;
-	private String userName;
-	private char[] password;
 
-	public JFireLoginAuthCallbackHandler(String organisationID, String userID, String sessionID, char[] password)
+	private LoginData loginData;
+	
+	public JFireLoginAuthCallbackHandler(LoginData loginData)
 	{
-		this.organisationID = organisationID;
-		this.userID = userID;
-		this.sessionID = sessionID;
-		this.password = password;
-		this.userName = this.userID + '@' + this.organisationID + '/' + this.sessionID;
+		this.loginData = loginData;
+//		String additionalQuery = "";
+//		if ( loginData.getAdditionalParams() != null && ! loginData.getAdditionalParams().isEmpty() )
+//			additionalQuery = loginData.getAdditionalParams().toURLQuery();
+		
+//		this.userName = loginData.getUsername() + '?' + additionalQuery ;
 	}
 
 	public void handle(Callback[] callbacks) throws IOException,
@@ -36,10 +37,10 @@ public class JFireLoginAuthCallbackHandler implements CallbackHandler
 		for (int i = 0; i < callbacks.length; ++i) {
 			Callback cb = callbacks[i];
 			if (cb instanceof NameCallback) {
-				((NameCallback)cb).setName(userName);
+				((NameCallback)cb).setName(loginData.getLoginDataURL());
 			}
 			else if (cb instanceof PasswordCallback) {
-				((PasswordCallback)cb).setPassword(password);
+				((PasswordCallback)cb).setPassword(loginData.getPassword().toCharArray());
 			}
 			else throw new UnsupportedCallbackException(cb);
 		}
