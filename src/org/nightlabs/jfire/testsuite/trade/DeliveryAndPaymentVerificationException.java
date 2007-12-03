de45f39e2cfbb79308f410bf227db37b9c906ce5
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-03 17:15:35 +0000

    moved trade related classes from JFireTestSuite to JFireTestSuiteTrade
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@9410 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/trade/DeliveryAndPaymentVerificationException.java
+++
@@ -1,9 +0,0 @@
-package org.nightlabs.jfire.testsuite.trade;
-
-public class DeliveryAndPaymentVerificationException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public DeliveryAndPaymentVerificationException(String message) {
-		super(message);
-	}
-}
