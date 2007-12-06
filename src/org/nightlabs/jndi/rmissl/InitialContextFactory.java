313d609c1a1f71222956d7b032d03789cf371d8b
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-06 18:29:27 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIntegrationJBoss@9492 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jndi/rmissl/InitialContextFactory.java
@@ -0,0 +1,35 @@
+package org.nightlabs.jndi.rmissl;
+
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NamingException;
+import org.jboss.security.jndi.*;
+
+public class InitialContextFactory extends LoginInitialContextFactory {
+
+	
+	
+	
+	public Context getInitialContext(Hashtable arg0)
+			throws NamingException {
+		// TODO Auto-generated method stub
+		return new NamingContext(super.getInitialContext(arg0));
+	
+	}
+
+	public Object getObjectInstance(Object obj, Name name, Context nameCtx,
+			Hashtable environment) throws Exception {
+		// TODO Auto-generated method stub
+		return super.getObjectInstance(obj, name, nameCtx, environment);
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/org/nightlabs/jndi/rmissl/NamingContext.java
@@ -0,0 +1,156 @@
+package org.nightlabs.jndi.rmissl;
+
+
+import java.util.Hashtable;
+import javax.naming.Binding;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NameClassPair;
+import javax.naming.NameParser;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+
+public class NamingContext implements Context {
+	private Context delegate;
+	
+	private static final String EJB_PREFIX = "jfire/ejb";
+	
+	private static final String EJB_SSLRMI_PREFIX = "ejbssl/";
+	
+	
+	public NamingContext(Context delegate) {
+		this.delegate = delegate;
+	}
+
+	public Object addToEnvironment(String propName, Object propVal)
+			throws NamingException {
+		return delegate.addToEnvironment(propName, propVal);
+	}
+
+	public void bind(Name name, Object obj) throws NamingException {
+		delegate.bind(name, obj);
+	}
+
+	public void bind(String name, Object obj) throws NamingException {
+		delegate.bind(name, obj);
+	}
+
+	public void close() throws NamingException {
+		delegate.close();
+	}
+
+	public Name composeName(Name name, Name prefix) throws NamingException {
+		return delegate.composeName(name, prefix);
+	}
+
+	public String composeName(String name, String prefix)
+			throws NamingException {
+		return delegate.composeName(name, prefix);
+	}
+
+	public Context createSubcontext(Name name) throws NamingException {
+		return delegate.createSubcontext(name);
+	}
+
+	public Context createSubcontext(String name) throws NamingException {
+		return delegate.createSubcontext(name);
+	}
+
+	public void destroySubcontext(Name name) throws NamingException {
+		delegate.destroySubcontext(name);
+	}
+
+	public void destroySubcontext(String name) throws NamingException {
+		delegate.destroySubcontext(name);
+	}
+
+	public Hashtable<?, ?> getEnvironment() throws NamingException {
+		return delegate.getEnvironment();
+	}
+
+	public String getNameInNamespace() throws NamingException {
+		return delegate.getNameInNamespace();
+	}
+
+	public NameParser getNameParser(Name name) throws NamingException {
+		return delegate.getNameParser(name);
+	}
+
+	public NameParser getNameParser(String name) throws NamingException {
+		return delegate.getNameParser(name);
+	}
+
+	public NamingEnumeration<NameClassPair> list(Name name)
+			throws NamingException {
+		return delegate.list(name);
+	}
+
+	public NamingEnumeration<NameClassPair> list(String name)
+			throws NamingException {
+		return delegate.list(name);
+	}
+
+	public NamingEnumeration<Binding> listBindings(Name name)
+			throws NamingException {
+		return delegate.listBindings(name);
+	}
+
+	public NamingEnumeration<Binding> listBindings(String name)
+			throws NamingException {
+		return delegate.listBindings(name);
+	}
+
+	public Object lookup(Name name) throws NamingException {
+	
+		
+		throw new UnsupportedOperationException("NYI");
+//		return delegate.lookup(name);
+	}
+
+	
+	public Object lookup(String name) throws NamingException {
+		
+		
+		if (name.startsWith(EJB_PREFIX))
+			name = EJB_SSLRMI_PREFIX + name;
+		
+		return delegate.lookup(name);
+	}
+
+	public Object lookupLink(Name name) throws NamingException {
+		return delegate.lookupLink(name);
+	}
+
+	public Object lookupLink(String name) throws NamingException {
+		return delegate.lookupLink(name);
+	}
+
+	public void rebind(Name name, Object obj) throws NamingException {
+		delegate.rebind(name, obj);
+	}
+
+	public void rebind(String name, Object obj) throws NamingException {
+		delegate.rebind(name, obj);
+	}
+
+	public Object removeFromEnvironment(String propName) throws NamingException {
+		return delegate.removeFromEnvironment(propName);
+	}
+
+	public void rename(Name oldName, Name newName) throws NamingException {
+		delegate.rename(oldName, newName);
+	}
+
+	public void rename(String oldName, String newName) throws NamingException {
+		delegate.rename(oldName, newName);
+	}
+
+	public void unbind(Name name) throws NamingException {
+		delegate.unbind(name);
+	}
+
+	public void unbind(String name) throws NamingException {
+		delegate.unbind(name);
+	}
+	
+}
