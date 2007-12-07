023a55f771c814fb7975ac1c79afbc8d7bb36900
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-12-07 18:03:31 +0000

    * new build-nightlabs.xml * switched version from 0.9.2 to 0.9.3 * switched ejb-prefix from crossticket/ejb to jfire/ejb * switched ejb-prefix from ipanema/ejb to jfire/ejb * renamed org.nightlabs.jfire.jboss.jndi.rmissl.InitialContextFactory to LoginInitialContextFactory
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIntegrationJBoss@9509 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jboss/jndi/rmissl/LoginInitialContextFactory.java
+++ src/org/nightlabs/jfire/jboss/jndi/rmissl/LoginInitialContextFactory.java
@@ -1,35 +1,27 @@
 package org.nightlabs.jfire.jboss.jndi.rmissl;
 
-
 import java.util.Hashtable;
 
 import javax.naming.Context;
 import javax.naming.Name;
 import javax.naming.NamingException;
-import org.jboss.security.jndi.*;
 
-public class InitialContextFactory extends LoginInitialContextFactory {
+public class LoginInitialContextFactory
+extends org.jboss.security.jndi.LoginInitialContextFactory
+{
 
-	
-	
-	
+	@SuppressWarnings("unchecked")
 	public Context getInitialContext(Hashtable arg0)
-			throws NamingException {
-		// TODO Auto-generated method stub
+	throws NamingException
+	{
 		return new NamingContext(super.getInitialContext(arg0));
-	
 	}
 
-	public Object getObjectInstance(Object obj, Name name, Context nameCtx,
-			Hashtable environment) throws Exception {
-		// TODO Auto-generated method stub
+	@SuppressWarnings("unchecked")
+	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable environment)
+	throws Exception
+	{
 		return super.getObjectInstance(obj, name, nameCtx, environment);
 	}
-	
-	
-	
-	
-	
-	
 
 }
