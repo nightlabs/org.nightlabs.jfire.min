fcbcd883c269ad2f155afd84788f81c4bba5a2c3
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-02-12 14:51:18 +0000

    Removed unused comments for serial version UIDs.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@9886 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/JFireTestManagerBean.java
+++ src/org/nightlabs/jfire/testsuite/JFireTestManagerBean.java
@@ -72,9 +72,6 @@ public abstract class JFireTestManagerBean
 extends BaseSessionBeanImpl
 implements SessionBean
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	/**
 	 * LOG4J logger used by this class
