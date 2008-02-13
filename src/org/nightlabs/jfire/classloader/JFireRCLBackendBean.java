ad2f84f8dad6a79cd026ed5fc222ee827cf7b27f
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-02-13 08:50:31 +0000

    Eclipse cleanup
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireRCLBackendBean@9897 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/classloader/JFireRCLBackendBean.java
+++ src/org/nightlabs/jfire/classloader/JFireRCLBackendBean.java
@@ -26,18 +26,14 @@
 
 package org.nightlabs.jfire.classloader;
 
-import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.List;
-import java.util.Map;
-
 import javax.ejb.CreateException;
 import javax.ejb.EJBException;
 import javax.ejb.SessionBean;
 import javax.ejb.SessionContext;
 
 import org.apache.log4j.Logger;
-import org.nightlabs.ModuleException;
 import org.nightlabs.jfire.base.BaseSessionBeanImpl;
 import org.nightlabs.jfire.servermanager.JFireServerManager;
 
@@ -71,7 +67,7 @@ public abstract class JFireRCLBackendBean
 	}
 
 	/**
-	 * @ejb.create-method  
+	 * @ejb.create-method
 	 * @ejb.permission role-name="_Guest_"
 	 */
 	public void ejbCreate()
