00f218495571f75d7d4af3e31acbdadf9a76c171
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-08 20:27:57 +0000

    creating persistence.xml before *-ds.xml
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireServerManager@10502 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/servermanager/ra/JFireServerManagerFactoryImpl.java
+++ src/org/nightlabs/jfire/servermanager/ra/JFireServerManagerFactoryImpl.java
@@ -1416,6 +1416,11 @@ public class JFireServerManagerFactoryImpl
 							// TODO this file needs to be configurable!
 							File persistenceXML = new File(jdoConfigDir, "persistence.xml");
 
+							// TODO this file needs to be configurable!!!
+							createDeploymentDescriptor(organisationID, persistenceXML,
+									new File(new File(jdoCf.getJdoTemplateDSXMLFile()).getParentFile(), "persistence.template.xml"),
+									null, DeployOverwriteBehaviour.EXCEPTION);
+							
 							// creating deployment descriptor for datasource
 							createDeploymentDescriptor(organisationID, datasourceDSXML,
 									new File(dbCf.getDatasourceTemplateDSXMLFile()), null, DeployOverwriteBehaviour.EXCEPTION);
@@ -1423,11 +1428,6 @@ public class JFireServerManagerFactoryImpl
 							// creating deployment descriptor for JDO PersistenceManagerFactory
 							createDeploymentDescriptor(organisationID, jdoDSXML,
 									new File(jdoCf.getJdoTemplateDSXMLFile()), null, DeployOverwriteBehaviour.EXCEPTION);
-
-							// TODO this file needs to be configurable!!!
-							createDeploymentDescriptor(organisationID, persistenceXML,
-									new File(new File(jdoCf.getJdoTemplateDSXMLFile()).getParentFile(), "persistence.template.xml"),
-									null, DeployOverwriteBehaviour.EXCEPTION);
 
 							organisationCf = organisationConfigModule.addOrganisation(
 									organisationID, organisationName);
