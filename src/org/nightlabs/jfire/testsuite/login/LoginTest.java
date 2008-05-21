37afdcb88831b1127df196ddd4326e2740d9ca34
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-21 21:51:49 +0000

    * continued work on Authority management UI * removed unnecessary method from ReflectUtil * made org.nightlabs.jfire.security.RoleManagerBean * merged the non-deprecated EJBs in org.nightlabs.jfire.security into one single EJB named JFireSecurityManager[Bean] * removed many unnecessary methods from JFireSecurityManager and some DAOs * made JFireLocale thread-safe
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@10587 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/login/LoginTest.java
+++ src/org/nightlabs/jfire/testsuite/login/LoginTest.java
@@ -8,7 +8,7 @@ import javax.security.auth.login.LoginException;
 import junit.framework.TestCase;
 
 import org.nightlabs.jfire.base.login.JFireLogin;
-import org.nightlabs.jfire.security.UserManagerUtil;
+import org.nightlabs.jfire.security.JFireSecurityManagerUtil;
 import org.nightlabs.jfire.testsuite.JFireTestSuite;
 
 /**
@@ -24,7 +24,7 @@ public class LoginTest extends TestCase
 	public void testCorrectLogin() throws Exception {
 		JFireLogin login = JFireTestLogin.getUserLogin(JFireTestLogin.USER_QUALIFIER_SERVER_ADMIN);
 		login.login(); // Warning! even though it's fine for testing here, switching the identity during a running transaction might cause trouble.
-		UserManagerUtil.getHome(login.getInitialContextProperties()).create();
+		JFireSecurityManagerUtil.getHome(login.getInitialContextProperties()).create();
 	}
 
 	public void testIncorrectLogin() {
@@ -36,7 +36,7 @@ public class LoginTest extends TestCase
 			return;
 		}
 		fail("Could login with wrong credentials");
-//		UserManagerUtil.getHome(login.getInitialContextProperties()).create();
+//		JFireSecurityManagerUtil.getHome(login.getInitialContextProperties()).create();
 	}
 
 }
