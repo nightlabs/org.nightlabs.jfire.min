880f91d80d12c5a05bab8104e03f0906f2caaff4
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-05-22 22:02:12 +0000

    * continued work on Authority editor page and related access right management * made some properties of the JDOCacheBridgeDefault configurable
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseBean@10596 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/security/RoleConstants.java
+++ src/org/nightlabs/jfire/security/RoleConstants.java
@@ -1,9 +1,11 @@
 package org.nightlabs.jfire.security;
 
+import org.nightlabs.jfire.security.id.RoleID;
+
 public class RoleConstants {
 	private RoleConstants() {}
 
-	public static final String securityManager_getRoleGroupIDSetCarrier = JFireSecurityManager.class.getName() + "#getRoleGroupIDSetCarrier";
-	public static final String securityManager_removeRoleGroupFromUser = JFireSecurityManager.class.getName() + "#removeRoleGroupFromUser";
-	public static final String securityManager_addRoleGroupToUser = JFireSecurityManager.class.getName() + "#addRoleGroupToUser";
+	public static final RoleID securityManager_getRoleGroupIDSetCarrier = RoleID.create(JFireSecurityManager.class.getName() + "#getRoleGroupIDSetCarrier");
+	public static final RoleID securityManager_removeRoleGroupFromUser = RoleID.create(JFireSecurityManager.class.getName() + "#removeRoleGroupFromUser");
+	public static final RoleID securityManager_addRoleGroupToUser = RoleID.create(JFireSecurityManager.class.getName() + "#addRoleGroupToUser");
 }
