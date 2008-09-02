caa4cf1909964c43f8e7e04bc38bc89bbf321604
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-09-02 16:55:58 +0000

    - reduced the logging level for some messages to debug or trace - fixed a bug in Version that prohibited to have an Osgi-Version with a suffix consisting of numbers > Integer.MaxValue - fixed a bug in NLJDOUtil.getObjectList that resulted in fetching wrong objects and hence throwing an exception where null should have been returned
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseBean@11995 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/organisationinit/OrganisationInitDelegateBean.java
+++ src/org/nightlabs/jfire/organisationinit/OrganisationInitDelegateBean.java
@@ -81,7 +81,8 @@ implements SessionBean
 		Logger logger = Logger.getLogger(OrganisationInitDelegateBean.class);
 		InitialContext initCtx = new InitialContext();
 		try {
-			logger.info("Executing OrganisationInit as user " + getPrincipal() +": " + init);
+			if (logger.isDebugEnabled())
+				logger.debug("Executing OrganisationInit as user " + getPrincipal() +": " + init);
 			Object bean = InvokeUtil.createBean(initCtx, init.getBean());
 			Method beanMethod = bean.getClass().getMethod(init.getMethod(), (Class[]) null);
 			beanMethod.invoke(bean, (Object[]) null);
@@ -102,7 +103,8 @@ implements SessionBean
 		Logger logger = Logger.getLogger(OrganisationInitDelegateBean.class);
 		InitialContext initCtx = new InitialContext();
 		try {
-			logger.info("Executing CrossOrganisationRegistrationInit as user " + getPrincipal() +": " + init);
+			if (logger.isDebugEnabled())
+				logger.debug("Executing CrossOrganisationRegistrationInit as user " + getPrincipal() +": " + init);
 			Object bean = InvokeUtil.createBean(initCtx, init.getBean());
 			Method beanMethod = bean.getClass().getMethod(init.getMethod(), new Class[] { Context.class });
 			beanMethod.invoke(bean, new Object[] { context });
