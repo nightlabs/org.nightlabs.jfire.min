d6ac97959bb3efac63b1a230b4164fe587dee200
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-10-28 09:32:20 +0000

    Mail config for smtps with authentication
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireServerManager@12521 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/servermanager/config/SmtpMailServiceCf.java
+++ src/org/nightlabs/jfire/servermanager/config/SmtpMailServiceCf.java
@@ -1,17 +1,25 @@
 package org.nightlabs.jfire.servermanager.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.nightlabs.config.InitException;
 
 /**
  * @author Marc Klinger - marc[at]nightlabs[dot]de
  * @author Khaled Soliman
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
  */
 public class SmtpMailServiceCf extends JFireServerConfigPart
 {
+	public static final int DEFAULT_PORT_PLAIN = 25;
+	public static final int DEFAULT_PORT_SSL = 465;
+
+
 	/**
 	 * The serial version of this class.
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 20081028L;
 
 	public static final String ENCRYPTION_METHOD_NONE = "none";
 	public static final String ENCRYPTION_METHOD_SSL = "ssl";
@@ -40,9 +48,9 @@ public class SmtpMailServiceCf extends JFireServerConfigPart
 			encryptionMethod = ENCRYPTION_METHOD_NONE;
 		if(port == null) {
 			if(ENCRYPTION_METHOD_SSL.equals(encryptionMethod))
-				port = 443;
+				port = DEFAULT_PORT_SSL;
 			else
-				port = 25;
+				port = DEFAULT_PORT_PLAIN;
 		}
 		if(mailFrom == null)
 			mailFrom = "nobody@nosuchhost.nosuchdomain.com";
@@ -190,5 +198,25 @@ public class SmtpMailServiceCf extends JFireServerConfigPart
 	public void setUsername(String username) {
 		this.username = username;
 		setChanged();
+	}
+	
+	public Map<String, String> createProperties() {
+		Map<String, String> props = new HashMap<String, String>();
+		props.put("mail.smtp.host", getHost());
+		props.put("mail.smtp.port", String.valueOf(getPort()));
+		props.put("mail.from", getMailFrom());
+		props.put("mail.debug", String.valueOf(getDebug()));
+		if (getUseAuthentication()) {
+			props.put("mail.smtp.auth", "true");			
+			props.put("mail.smtp.user", getUsername());
+		}
+		if (ENCRYPTION_METHOD_SSL.equals(getEncryptionMethod())) {
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.socketFactory.port", String.valueOf(getPort()));
+		} 
+		if (ENCRYPTION_METHOD_TLS.equals(getEncryptionMethod())) {
+			props.put("mail.smtp.starttls.enable", "true");
+		}
+		return props;
 	}
 }
