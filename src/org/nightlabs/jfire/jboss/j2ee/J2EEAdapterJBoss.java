6d676b70979cbbbd0e2f19c5024a6f57a34e6660
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-11-18 12:59:31 +0000

    https://www.jfire.org/modules/bugs/view.php?id=112 removed all references to J2EEServerMonitor
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireIntegrationJBoss@12819 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/jboss/j2ee/J2EEAdapterJBoss.java
+++ src/org/nightlabs/jfire/jboss/j2ee/J2EEAdapterJBoss.java
@@ -37,11 +37,9 @@ import javax.management.ObjectName;
 import javax.naming.InitialContext;
 import javax.transaction.UserTransaction;
 
-import org.nightlabs.jfire.jboss.j2ee.monitor.J2EEServerMonitorJBoss;
 import org.nightlabs.jfire.security.SecurityReflector;
 import org.nightlabs.jfire.servermanager.j2ee.J2EEAdapter;
 import org.nightlabs.jfire.servermanager.j2ee.ServerStartNotificationListener;
-import org.nightlabs.jfire.servermanager.j2ee.monitor.J2EEServerMonitor;
 
 /**
  * @author Marco Schulze - marco at nightlabs dot de
@@ -155,18 +153,5 @@ public class J2EEAdapterJBoss implements J2EEAdapter
 	public void reboot()
 	{
 		System.exit(10);
-	}
-
-	private J2EEServerMonitorJBoss serverMonitor;
-
-	@Override
-	public J2EEServerMonitor getServerMonitor() {
-		if (serverMonitor == null) {
-			synchronized (J2EEAdapterJBoss.class) {
-				if (serverMonitor == null)
-					serverMonitor = new J2EEServerMonitorJBoss();
-			}
-		}
-		return serverMonitor;
 	}
 }
