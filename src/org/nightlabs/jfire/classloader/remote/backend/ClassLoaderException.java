049177dd82572b32fde53f11be8f99026c760339
marc <marc@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-24 20:03:21 +0000

    project renamed
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.classloader.remote.backend@14641 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/classloader/remote/backend/ClassLoaderException.java
@@ -0,0 +1,63 @@
+/* *****************************************************************************
+ * JFire - it's hot - Free ERP System - http://jfire.org                       *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://opensource.org/licenses/lgpl-license.php                         *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
+package org.nightlabs.jfire.classloader.remote.backend;
+
+/**
+ * @author marco
+ */
+public class ClassLoaderException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public ClassLoaderException() { }
+
+	/**
+	 * @param message
+	 */
+	public ClassLoaderException(String message)
+	{
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ClassLoaderException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ClassLoaderException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+}
