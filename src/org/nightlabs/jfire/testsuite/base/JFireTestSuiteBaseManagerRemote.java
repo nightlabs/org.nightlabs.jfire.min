042b5d018668184bf889fe02213a639e500a5f0e
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-08-07 05:13:04 +0000

    added a new test case for creating a dummy user and a person.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@15343 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/testsuite/base/JFireTestSuiteBaseManagerRemote.java
@@ -0,0 +1,44 @@
+package org.nightlabs.jfire.testsuite.base;
+
+import java.util.Date;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Remote;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import org.nightlabs.jfire.person.Person;
+import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.trade.LegalEntity;
+
+@Remote
+public interface JFireTestSuiteBaseManagerRemote {
+
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_System_")
+	void initialise() throws Exception;
+	
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_System_")		
+	public User createUser(String userID, String password, Person person) throws Exception;
+
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_System_")
+	public Person createPerson(String company, String name, String firstName, String eMail,
+			Date dateOfBirth, String salutation, String title, String postAdress, String postCode,
+			String postCity, String postRegion, String postCountry, String phoneCountryCode,
+			String phoneAreaCode, String phoneNumber, String faxCountryCode,
+			String faxAreaCode, String faxNumber, String bankAccountHolder, String bankAccountNumber,
+			String bankCode, String bankName, String creditCardHolder, String creditCardNumber,
+			int creditCardExpiryMonth, int creditCardExpiryYear, String comment)
+	throws Exception;
+	
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	@RolesAllowed("_System_")
+	public LegalEntity createLegalEntity(Person person)
+	throws Exception;
+
+}
--- src/org/nightlabs/jfire/testsuite/base/UserTest.java
+++ src/org/nightlabs/jfire/testsuite/base/UserTest.java
@@ -3,11 +3,28 @@
  */
 package org.nightlabs.jfire.testsuite.base;
 
+import java.rmi.RemoteException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.ejb.CreateException;
+import javax.naming.NamingException;
+
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.nightlabs.jfire.base.JFireEjb3Factory;
+import org.nightlabs.jfire.idgenerator.IDGenerator;
+import org.nightlabs.jfire.person.Person;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.security.User;
 import org.nightlabs.jfire.testsuite.JFireTestSuite;
+import org.nightlabs.jfire.trade.LegalEntity;
+import org.nightlabs.jfire.trade.Order;
 
 /**
  * A simple TestCase for demonstration.
@@ -37,12 +54,81 @@ public class UserTest extends TestCase
 	 * This method is invoked by the JUnit run,
 	 * as its name starts with test!
 	 */
-	public void testCreateUser() {
+	public void testCreateUser() throws Exception{		
+		
+		long ID = IDGenerator.nextID(User.class);
+		
+		// Person 's information.
+		String company = "Company";
+		String name = "Name"+ID;
+		String firstName = "Firstname";
+		String eMail = "email";
+		String dateOfBirth = "";
+		String salutation = "Mr";
+		String title = "Mr";
+		String postAdress = "4B strasse";
+		String postCode = "478541";
+		String postCity = "Berlin";
+		String postRegion = "";
+		String postCountry = "Germany";
+		String phoneCountryCode = "78";
+		String phoneAreaCode = "41";
+		String phoneNumber = "44444444";
+		String faxCountryCode = "555555";
+		String faxAreaCode = "55";
+		String faxNumber = "8888888";
+		String bankAccountHolder = "Name"+ID;
+		String bankAccountNumber = "45115-54557";
+		String bankCode = "code-111";
+		String bankName = "Bank of Germany";
+		String creditCardHolder = "Name"+ID;
+		String creditCardNumber = "4551-4477-11447";
+		int creditCardExpiryMonth = 11;
+		int creditCardExpiryYear = 2020;
+		String comment = "";	
+
+		DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+		Date dob;
+		try { dob = formatter.parse( dateOfBirth ); }
+		catch (ParseException e) { dob = new Date(); }
+		
+		
+		logger.info("test Create Person: begin");		
+		
+		// Create the Person (a person in a JFire datastore).
+		Person person = getJFireTestSuiteBaseManager().createPerson(
+				           company, name, firstName, eMail, dob, salutation, title, postAdress, postCode,
+				           postCity, postRegion, postCountry, phoneCountryCode, phoneAreaCode, phoneNumber, faxCountryCode,
+				           faxAreaCode, faxNumber, bankAccountHolder, bankAccountNumber, bankCode, bankName, creditCardHolder, creditCardNumber,
+				           creditCardExpiryMonth, creditCardExpiryYear, comment);
+		
+		
+		logger.info("test Create Person: end");
+
+		logger.info("test Create LegalEntity: begin");
+		LegalEntity legalEntity = getJFireTestSuiteBaseManager().createLegalEntity(person);
+		
+		logger.info("test Create LegalEntity: end");
+		
 		logger.info("testCreateUser: begin");
+				
+		String userID = "User"+String.valueOf(ID);
+		String password = "test";
+		User user = getJFireTestSuiteBaseManager().createUser(userID, password, person);			
+		
 		// TODO implement!
 		// It does not do very much, though
 		logger.info("testCreateUser: end");
 	}
+	
+	
+	private JFireTestSuiteBaseManagerRemote getJFireTestSuiteBaseManager() throws RemoteException, CreateException, NamingException {
+		return JFireEjb3Factory.getRemoteBean(JFireTestSuiteBaseManagerRemote.class, SecurityReflector.getInitialContextProperties());
+	}
+	
+	
+	
+	
 	
 	/**
 	 * This method is invoked by the JUnit run,
