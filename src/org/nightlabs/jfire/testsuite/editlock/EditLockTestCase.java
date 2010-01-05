ac2eea9310dd454c7afdcdc10d0231faf7b4d0d3
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-05 22:31:53 +0000

    the EditLockTestCase has been rewritten again to use the interface methods properly
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuiteJFireBase@16552 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/editlock/EditLockTestCase.java
+++ src/org/nightlabs/jfire/testsuite/editlock/EditLockTestCase.java
@@ -1,125 +1,124 @@
 package org.nightlabs.jfire.testsuite.editlock;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.Collection;
+import java.util.Set;
 
 import javax.jdo.FetchPlan;
 import javax.jdo.JDOHelper;
+import javax.security.auth.login.LoginException;
 
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.nightlabs.jdo.NLJDOHelper;
-import org.nightlabs.jdo.query.QueryCollection;
 import org.nightlabs.jfire.base.JFireBaseEAR;
 import org.nightlabs.jfire.base.JFireEjb3Factory;
+import org.nightlabs.jfire.base.login.JFireLogin;
 import org.nightlabs.jfire.editlock.AcquireEditLockResult;
 import org.nightlabs.jfire.editlock.EditLock;
 import org.nightlabs.jfire.editlock.EditLockManagerRemote;
+import org.nightlabs.jfire.editlock.ReleaseReason;
+import org.nightlabs.jfire.editlock.id.EditLockID;
 import org.nightlabs.jfire.idgenerator.IDGenerator;
 import org.nightlabs.jfire.person.Person;
 import org.nightlabs.jfire.person.PersonStruct;
 import org.nightlabs.jfire.prop.IStruct;
 import org.nightlabs.jfire.prop.PropertyManagerRemote;
 import org.nightlabs.jfire.prop.PropertySet;
-import org.nightlabs.jfire.prop.exception.DataBlockGroupNotFoundException;
-import org.nightlabs.jfire.prop.exception.DataBlockNotFoundException;
-import org.nightlabs.jfire.prop.exception.DataFieldNotFoundException;
 import org.nightlabs.jfire.prop.id.PropertySetID;
 import org.nightlabs.jfire.security.SecurityReflector;
 import org.nightlabs.jfire.testsuite.JFireTestSuite;
-import org.nightlabs.jfire.workstation.Workstation;
-import org.nightlabs.jfire.workstation.WorkstationManagerRemote;
-import org.nightlabs.jfire.workstation.id.WorkstationID;
-import org.nightlabs.jfire.workstation.search.WorkstationQuery;
 
 
-
+/**
+ * 
+ * this Test Case tests all the possible API used in the EditLockManager. 
+ * 
+ * the following EJB methods has been tested and Called
+ * 
+ * 	AcquireEditLockResult acquireEditLock(EditLockTypeID editLockTypeID,
+ *			ObjectID objectID, String description, String[] fetchGroups,
+ *			int maxFetchDepth);
+ *
+ *	void releaseEditLock(ObjectID objectID, ReleaseReason releaseReason);
+ *
+ *	Set<EditLockID> getEditLockIDs(ObjectID objectID);
+ *
+ * 	List<EditLock> getEditLocks(Collection<EditLockID> editLockIDs,
+ *			String[] fetchGroups, int maxFetchDepth);
+ * 
+ * 
+ *  @author Fitas Amine - fitas [at] nightlabs [dot] de
+ */
 @JFireTestSuite(JFireBaseEditLockTestSuite.class)
 public class EditLockTestCase extends TestCase{
-
 	Logger logger = Logger.getLogger(EditLockTestCase.class);	
+	private static ThreadLocal<PropertySetID> newObjectforEditLock = new ThreadLocal<PropertySetID>();
+	private static String[] FETCH_GROUP_EDITLOCK = new String[]{
+		FetchPlan.DEFAULT,
+		EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
+		EditLock.FETCH_GROUP_EDIT_LOCK_TYPE	
+	};	
 
-	@Test
-	public void testEditLock() throws Exception{	
-
-
-		// create a dummy person and link it to a new Issue
-		PropertyManagerRemote pm = JFireEjb3Factory.getRemoteBean(PropertyManagerRemote.class, SecurityReflector.getInitialContextProperties());
-
-		PropertySetID ObjectEditLock = creatDemoObjectforLock();	
-		EditLockManagerRemote wm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
-		WorkstationManagerRemote ws = JFireEjb3Factory.getRemoteBean(WorkstationManagerRemote.class, SecurityReflector.getInitialContextProperties());
-
-
-		AcquireEditLockResult acquireEditLockResult = wm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
+	@Override
+	protected void setUp()throws Exception
+	{
+		super.setUp();
+		// create a dummy Object to Test Edit Locking
+		PropertySetID ObjectEditLock = createDemoObjectForEditLocking();	
+		EditLockManagerRemote elm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		newObjectforEditLock.set(ObjectEditLock);
+		elm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
 				ObjectEditLock, 
-				"testLock", 
-				new String[] {FetchPlan.DEFAULT, 
-				EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
-				EditLock.FETCH_GROUP_EDIT_LOCK_TYPE}, 
-				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-
-
-		WorkstationID randomWsID = getRandomWorkstation();
-
-		acquireEditLockResult = wm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
-				randomWsID, 
-				"testLock", 
-				new String[] {FetchPlan.DEFAULT, 
-				EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
-				EditLock.FETCH_GROUP_EDIT_LOCK_TYPE}, 
-				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-
-
-		Workstation wsObject = ws.getWorkstations(Collections.singleton(randomWsID), 
-				new String[] {FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT).iterator().next();
-
-		wsObject.setDescription("Edit the workstation");
-
-		ws.storeWorkstation(wsObject,false, null, -1);
-
-		Person person = (Person) pm.getPropertySet(ObjectEditLock, 
-				new String[] {FetchPlan.DEFAULT, Person.FETCH_GROUP_FULL_DATA, Person.FETCH_GROUP_DATA_FIELDS}, 
-				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-
-		IStruct personStruct  = pm.getFullStructLocal(person.getStructLocalObjectID(), 
-				new String[] {FetchPlan.DEFAULT, IStruct.FETCH_GROUP_ISTRUCT_FULL_DATA}, 
-				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-		person.inflate(personStruct);		
-		person.getDataField(PersonStruct.PERSONALDATA_NAME).setData("Modified Edit Lock");
-		person.deflate();
-
-		person = (Person) pm.storePropertySet(person, true, null, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
-
+				"testEditLock-1", 
+				FETCH_GROUP_EDITLOCK, 
+				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
 	}
-
-
-	private WorkstationID getRandomWorkstation(){	
-		WorkstationManagerRemote wm = JFireEjb3Factory.getRemoteBean(WorkstationManagerRemote.class, SecurityReflector.getInitialContextProperties());
-
-		final QueryCollection<WorkstationQuery> queries =new QueryCollection<WorkstationQuery>(Workstation.class);
-		WorkstationQuery WorkstationQuery = new WorkstationQuery();
-		queries.add(WorkstationQuery);
-		List<WorkstationID> workstationIDs = new ArrayList<WorkstationID>( wm.getWorkstationIDs(queries));
-		Random rndGen = new Random(System.currentTimeMillis());		
-
-		if (workstationIDs != null && !workstationIDs.isEmpty()) {
-			return workstationIDs.get(rndGen.nextInt(workstationIDs.size()));	
+	@Test
+	public void testDetectEditLockCollision() throws Exception{	
+		{
+			if (newObjectforEditLock.get() == null) {
+				fail("Seems that creating the dummy EditLock Object failed, no PropertySetID in the ThreadLocal");
+			}
+			// login with another User to detect the Collision.
+			JFireLogin login = new JFireLogin("chezfrancois.jfire.org", "francois", "test");
+			try {
+				login.login();
+				EditLockManagerRemote elm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
+				AcquireEditLockResult acquireEditLockResult = elm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
+						newObjectforEditLock.get(), 
+						"testEditLock-francois", 
+						new String[] {FetchPlan.DEFAULT, 
+					EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
+					EditLock.FETCH_GROUP_EDIT_LOCK_TYPE}, 
+					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
+				if(acquireEditLockResult.getEditLockCount()!= 2)
+					fail("the Object should have been locked already !!!!");				
+				Set<EditLockID> editLockIDs =  elm.getEditLockIDs(newObjectforEditLock.get());
+				logger.info("our Object Got EditLocks" + editLockIDs.size());
+				Collection<EditLock> editLocks = elm.getEditLocks(editLockIDs, 
+						FETCH_GROUP_EDITLOCK, 
+						NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+				// list all EditLocks by other Users
+				logger.info("the following locks has been found !!!");
+				for (EditLock editLock : editLocks) {
+					logger.info("Description :" + editLock.getDescription());
+					logger.info("Time Stamp Creation :" + editLock.getCreateDT().getTime());
+					logger.info("User :" + editLock.getLockOwnerUser().getName());
+					logger.info("Locked-Object-ID :" + editLock.getLockedObjectIDStr());
+					logger.info("-------------------------------------------");
+				}
+			}catch (LoginException e) {
+				fail("couldnt log with another User Account to test the EditLock!!!!");		
+			}
+			login.logout();
 		}
-
-		return null;
-	}	
-
-	private PropertySetID creatDemoObjectforLock() throws DataBlockNotFoundException, DataBlockGroupNotFoundException, DataFieldNotFoundException{	
+	}
+	private PropertySetID createDemoObjectForEditLocking() throws Exception{	
 		logger.info("test Create IssueLink: begin");
-
 		// create a dummy person and link it to a new Issue
 		PropertyManagerRemote pm = JFireEjb3Factory.getRemoteBean(PropertyManagerRemote.class, SecurityReflector.getInitialContextProperties());
-
 		Person person = new Person(IDGenerator.getOrganisationID(), IDGenerator.nextID(PropertySet.class));		
 		IStruct personStruct  = pm.getFullStructLocal(person.getStructLocalObjectID(), 
 				new String[] {FetchPlan.DEFAULT, IStruct.FETCH_GROUP_ISTRUCT_FULL_DATA}, 
@@ -130,11 +129,15 @@ public class EditLockTestCase extends TestCase{
 		person.setAutoGenerateDisplayName(true);
 		person.setDisplayName(null, personStruct);
 		person.deflate();
-
 		person = (Person) pm.storePropertySet(person, true, null, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
 		return (PropertySetID)JDOHelper.getObjectId(person);
-
-
+	}
+	@Override
+	protected void tearDown()throws Exception
+	{
+		super.tearDown();
+		EditLockManagerRemote elm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		elm.releaseEditLock(newObjectforEditLock.get(), ReleaseReason.normal);
 	}
 }
 
