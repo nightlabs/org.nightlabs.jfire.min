337adada078da5dea376a5c872b0442b86abaf11
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-07 08:16:07 +0000

    - added the possibility to fetch a subset of a PropertySets' field values.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireBaseDAO@16573 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/prop/dao/TrimmedPropertySetDAO.java
@@ -0,0 +1,159 @@
+package org.nightlabs.jfire.prop.dao;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.nightlabs.jdo.ObjectIDUtil;
+import org.nightlabs.jfire.base.JFireEjb3Factory;
+import org.nightlabs.jfire.base.jdo.BaseJDOObjectDAO;
+import org.nightlabs.jfire.prop.DataField;
+import org.nightlabs.jfire.prop.PropertyManagerRemote;
+import org.nightlabs.jfire.prop.PropertySet;
+import org.nightlabs.jfire.prop.Struct;
+import org.nightlabs.jfire.prop.StructField;
+import org.nightlabs.jfire.prop.id.PropertySetID;
+import org.nightlabs.jfire.prop.id.StructFieldID;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.progress.ProgressMonitor;
+import org.nightlabs.util.CollectionUtil;
+
+/**
+ * Data access objects for {@link PropertySet}s whose {@link DataField}s are trimmed
+ * to a subsect of the {@link StructField}s of the underlying {@link Struct}.
+ * 
+ * @author Tobias Langner <!-- tobias[dot]langner[at]nightlabs[dot]de -->
+ */
+public class TrimmedPropertySetDAO
+extends BaseJDOObjectDAO<PropertySetID, PropertySet>
+{
+
+	private static final String TRIMMED_PROPERTY_SETS_SCOPE = "org.nightlabs.jfire.prop.PropertyManagerRemote#getDetachedTrimmedPropertySets";
+
+	protected TrimmedPropertySetDAO() {}
+
+	/** The shared instance */
+	private static TrimmedPropertySetDAO sharedInstance = null;
+
+	/**
+	 * Returns (and lazily creates) the static shared instance of {@link TrimmedPropertySetDAO}.
+	 * @return The static shared instance of {@link TrimmedPropertySetDAO}.
+	 */
+	public static TrimmedPropertySetDAO sharedInstance() {
+		if (sharedInstance == null) {
+			synchronized (PropertySetDAO.class) {
+				if (sharedInstance == null)
+					sharedInstance = new TrimmedPropertySetDAO();
+			}
+		}
+		return sharedInstance;
+	}
+	
+	@Override
+	protected Collection<? extends PropertySet> retrieveJDOObjects(Set<PropertySetID> objectIDs, String[] fetchGroupArray, int maxFetchDepth,
+			ProgressMonitor monitor) throws Exception
+	{
+		List<String> fetchGroupList = CollectionUtil.array2ArrayList(fetchGroupArray);
+		PropertyManagerRemote pm = JFireEjb3Factory.getRemoteBean(PropertyManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		
+		// Extract StructFieldIDs encoded within the fetchGroups and remove them from the fetch groups afterwards
+		Set<StructFieldID> structFieldIDs = new HashSet<StructFieldID>();
+		
+		final String JDO_PREFIX = ObjectIDUtil.JDO_PREFIX + ObjectIDUtil.JDO_PREFIX_SEPARATOR;
+		
+		for (Iterator<String> it = fetchGroupList.iterator(); it.hasNext(); ) {
+			String fetchGroup = it.next();
+			
+			if (fetchGroup.startsWith(JDO_PREFIX)) {
+				structFieldIDs.add((StructFieldID) ObjectIDUtil.createObjectID(fetchGroup));
+				it.remove();
+			}
+		}
+		
+		String[] fetchGroups = CollectionUtil.collection2TypedArray(fetchGroupList, String.class);
+			
+		return pm.getDetachedTrimmedPropertySets(objectIDs, structFieldIDs, fetchGroups, maxFetchDepth);
+	}
+	
+	protected static String[] getMergedFetchGroups(String[] fetchGroups, Collection<StructFieldID> structFieldIDs) {
+		String[] result = Arrays.copyOf(fetchGroups, fetchGroups.length + structFieldIDs.size());
+		int i = fetchGroups.length;
+		
+		for (StructFieldID id : structFieldIDs)
+			result[i++] = id.toString();
+		
+		return result;
+	}
+	
+	/**
+	 * Returns the {@link PropertySet} with the given ID trimmed to the {@link DataField}s of the given {@link StructFieldID}s.
+	 * 
+	 * @param propertySetId The {@link PropertySetID} of the {@link PropertySet} to be returned.
+	 * @param structFieldIDs The IDs of the {@link StructField}s to which the returned {@link PropertySet} shall be trimmed.
+	 * @param fetchGroups The fetch groups to be used.
+	 * @param maxFetchDepth The max fetch depth to be used.
+	 * @param monitor A progress monitor.
+	 * @return The {@link PropertySet} with the given ID trimmed to the {@link DataField}s of the given {@link StructFieldID}s.
+	 */
+	public PropertySet getTrimmedPropertySet(PropertySetID propertySetId, Collection<StructFieldID> structFieldIDs, String[] fetchGroups,
+			int maxFetchDepth, ProgressMonitor monitor)
+	{
+		String[] mergedFetchGroups = getMergedFetchGroups(fetchGroups, structFieldIDs);
+		
+		return getJDOObject(TRIMMED_PROPERTY_SETS_SCOPE, propertySetId, mergedFetchGroups, maxFetchDepth, monitor);
+	}
+	
+	/**
+	 * Returns the {@link PropertySet}s with the given IDs trimmed to the {@link DataField}s of the given {@link StructFieldID}s.
+	 * 
+	 * @param propertySetIDs The {@link PropertySetID}s of the {@link PropertySet}s to be returned.
+	 * @param structFieldIDs The IDs of the {@link StructField}s to which the returned {@link PropertySet}s shall be trimmed.
+	 * @param fetchGroups The fetch groups to be used.
+	 * @param maxFetchDepth The max fetch depth to be used.
+	 * @param monitor A progress monitor.
+	 * @return The {@link PropertySet}s with the given IDs trimmed to the {@link DataField}s of the given {@link StructFieldID}s.
+	 */
+	public Collection<? extends PropertySet> getTrimmedPropertySets(Set<PropertySetID> propertySetIds, Collection<StructFieldID> structFieldIDs,
+			String[] fetchGroups, int maxFetchDepth, ProgressMonitor monitor)
+	{
+		String[] mergedFetchGroups = getMergedFetchGroups(fetchGroups, structFieldIDs);
+		
+		return getJDOObjects(TRIMMED_PROPERTY_SETS_SCOPE, propertySetIds, mergedFetchGroups, maxFetchDepth, monitor);
+	}
+	
+//	public static void main(String[] args) {
+//		StructFieldID fieldID1 = StructFieldID.create("sbo1", "sb1", "sfo1", "sf1");
+//		StructFieldID fieldID2 = StructFieldID.create("sbo1", "sb1", "sfo2", "sf2");
+//		StructFieldID fieldID3 = StructFieldID.create("sbo2", "sb2", "sfo1", "sf1");
+//		StructFieldID fieldID4 = StructFieldID.create("sbo2", "sb2", "sfo2", "sf2");
+//		Collection<StructFieldID> _structFieldIDs = new LinkedList<StructFieldID>();
+//		_structFieldIDs.add(fieldID1);
+//		_structFieldIDs.add(fieldID2);
+//		_structFieldIDs.add(fieldID3);
+//		_structFieldIDs.add(fieldID4);
+//		String[] fetchGroupArray = getMergedFetchGroups(new String[] { "fg1", "fg2", "fg3" }, _structFieldIDs);
+//		List<String> fetchGroupList = CollectionUtil.array2ArrayList(fetchGroupArray);
+//
+//		System.out.println(Arrays.toString(fetchGroupArray));
+//		Set<StructFieldID> structFieldIDs = new HashSet<StructFieldID>();
+//
+//		final String JDO_PREFIX = ObjectIDUtil.JDO_PREFIX + ObjectIDUtil.JDO_PREFIX_SEPARATOR;
+//
+//		for (Iterator<String> it = fetchGroupList.iterator(); it.hasNext(); ) {
+//			String fetchGroup = it.next();
+//
+//			if (fetchGroup.startsWith(JDO_PREFIX)) {
+//				structFieldIDs.add((StructFieldID) ObjectIDUtil.createObjectID(fetchGroup));
+//				it.remove();
+//			}
+//		}
+//
+//		String[] fetchGroups = CollectionUtil.collection2TypedArray(fetchGroupList, String.class);
+//
+//		System.out.println(Arrays.toString(fetchGroups));
+//		System.out.println(structFieldIDs.toString());
+//	}
+}
