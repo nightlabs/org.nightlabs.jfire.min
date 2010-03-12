c0b255de145b89c6332e424965b655482c9da2d1
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-03-12 17:45:54 +0000

    Some code review.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuite@17094 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/jdo/JDOTestCase.java
+++ src/org/nightlabs/jfire/testsuite/jdo/JDOTestCase.java
@@ -32,6 +32,8 @@ public class JDOTestCase extends TestCase
 	public void testCreateArrayListFromQueryResult() throws Exception
 	{
 		// REV Alex: What is this testing?!?
+		// REV Marco: This was IMHO written by me a long time ago because of a bug in DataNucleus when
+		// creating certain collections from query results.
 		LoginData loginData = JFireTestLogin.getUserLoginData(JFireTestLogin.USER_QUALIFIER_SERVER_ADMIN);
 		JFireEjb3Factory.getRemoteBean(JDOTestRemote.class, loginData.getInitialContextProperties()).createArrayListFromQueryResult();
 	}
@@ -39,6 +41,8 @@ public class JDOTestCase extends TestCase
 	public void testCreateHashSetFromQueryResult() throws Exception
 	{
 		// REV Alex: What is this testing?!?
+		// REV Marco: This was IMHO written by me a long time ago because of a bug in DataNucleus when
+		// creating certain collections from query results.
 		LoginData loginData = JFireTestLogin.getUserLoginData(JFireTestLogin.USER_QUALIFIER_SERVER_ADMIN);
 		JFireEjb3Factory.getRemoteBean(JDOTestRemote.class, loginData.getInitialContextProperties()).createHashSetFromQueryResult();
 	}
