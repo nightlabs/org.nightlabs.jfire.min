9a7db184dbbaa782340b965e64a7f678a974c7b5
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-03-22 10:01:36 +0000

    fixed EditLockTestCase and LanguageTestcase to newer test code review standard
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuiteJFireBase@17165 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/testsuite/editlock/EditLockTestCase.java
+++ src/org/nightlabs/jfire/testsuite/editlock/EditLockTestCase.java
@@ -1,13 +1,14 @@
 package org.nightlabs.jfire.testsuite.editlock;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
 import java.util.Collection;
 import java.util.Set;
 
 import javax.jdo.FetchPlan;
 import javax.jdo.JDOHelper;
 import javax.security.auth.login.LoginException;
-
-import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -28,8 +29,9 @@ import org.nightlabs.jfire.prop.PropertyManagerRemote;
 import org.nightlabs.jfire.prop.PropertySet;
 import org.nightlabs.jfire.prop.id.PropertySetID;
 import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.testsuite.JFireTestManagerRemote;
 import org.nightlabs.jfire.testsuite.JFireTestSuite;
-
+import org.nightlabs.jfire.testsuite.TestCase;
 
 /**
  * 
@@ -54,7 +56,6 @@ import org.nightlabs.jfire.testsuite.JFireTestSuite;
 @JFireTestSuite(JFireBaseEditLockTestSuite.class)
 public class EditLockTestCase extends TestCase{
 	Logger logger = Logger.getLogger(EditLockTestCase.class);	
-	private static ThreadLocal<PropertySetID> newObjectforEditLock = new ThreadLocal<PropertySetID>();
 	public static final String USER_ID = "francois";
 	public static final String USER_PASSWORD = "test";
 	public static final String ORGANISATION = "chezfrancois.jfire.org";
@@ -63,65 +64,59 @@ public class EditLockTestCase extends TestCase{
 		EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
 		EditLock.FETCH_GROUP_EDIT_LOCK_TYPE	
 	};	
-
-	@Override
-	protected void setUp()throws Exception
-	{
-		super.setUp();
+	
+	private static String NEWOBJECT_EDITLOCK = "NEWOBJECTFOREDITLOCK";
+	
+    protected void setUpBeforeClass() throws Exception
+    {
 		// create a dummy Object to Test EditLocking on it
-		PropertySetID ObjectEditLock = createDemoObjectForEditLocking();	
-		newObjectforEditLock.set(ObjectEditLock);
-		
-		// REV Alex: As far as I know setUp is called once before every
-		// test-method, not once per test. Fitas, were you aware of this when
-		// writing this test?
+		setTestCaseContextObject(NEWOBJECT_EDITLOCK, (PropertySetID)createDemoObjectForEditLocking());
 	}
 
 	@Test
 	public void testUserEditLockingAndReleasing() throws Exception{	
-		if (newObjectforEditLock.get() == null) {
-			fail("Seems that creating the dummy EditLock Object failed, no PropertySetID in the ThreadLocal");
-		}
+		
+		PropertySetID newObjectforEditLock = (PropertySetID)getTestCaseContextObject(NEWOBJECT_EDITLOCK);
+		assertThat(newObjectforEditLock,notNullValue()); 
 		EditLockManagerRemote elm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
+
+		JFireTestManagerRemote tm = JFireEjb3Factory.getRemoteBean(JFireTestManagerRemote.class, SecurityReflector.getInitialContextProperties());
+//		
+//		
 		// lock and release the Object Multiple times
 		for (int i = 0; i < 20; ++i) {
 			elm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
-					newObjectforEditLock.get(), 
+					newObjectforEditLock, 
 					"testEditLock-1", 
 					FETCH_GROUP_EDITLOCK, 
 					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
-			elm.releaseEditLock(newObjectforEditLock.get(), ReleaseReason.normal);	
+			elm.releaseEditLock(newObjectforEditLock, ReleaseReason.normal);			
 		}
 		// login with another user and check if a lock is still placed on the Object 
 		JFireLogin login = new JFireLogin(ORGANISATION, USER_ID, USER_PASSWORD);
 		try {
 			login.login();
-			Set<EditLockID> editLockIDs = elm.getEditLockIDs(newObjectforEditLock.get());
+			Set<EditLockID> editLockIDs = elm.getEditLockIDs(newObjectforEditLock);
 			if(editLockIDs.size() > 0)
 				fail("no Locks should exist on the the Object!!!!");				
-			elm.releaseEditLock(newObjectforEditLock.get(), ReleaseReason.normal);			
+			elm.releaseEditLock(newObjectforEditLock, ReleaseReason.normal);			
 		}catch (LoginException e) {
 			fail("couldnt login with another User Account to test the EditLock!!!!");		
 		}
-		login.logout();
-		
-		// REV Alex: try-finally for the login missing. An error inbetween might
-		// have influence on other tests. 
-		
+		finally{
+			login.logout();
+		}
 		// REV Alex: After releasing, the test should also check if the lock was
 		// really removed from the datastore
-		
 	}
 	
 
 	@Test
 	public void testDetectEditLockCollision() throws Exception{	
-			if (newObjectforEditLock.get() == null) {
-				fail("Seems that creating the dummy EditLock Object failed, no PropertySetID in the ThreadLocal");
-			}
+			PropertySetID newObjectforEditLock = (PropertySetID)getTestCaseContextObject(NEWOBJECT_EDITLOCK);
 			EditLockManagerRemote elm = JFireEjb3Factory.getRemoteBean(EditLockManagerRemote.class, SecurityReflector.getInitialContextProperties());
 			elm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
-					newObjectforEditLock.get(), 
+					newObjectforEditLock, 
 					"testEditLock-1", 
 					FETCH_GROUP_EDITLOCK, 
 					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
@@ -130,15 +125,15 @@ public class EditLockTestCase extends TestCase{
 			try {
 				login.login();
 				AcquireEditLockResult acquireEditLockResult = elm.acquireEditLock(JFireBaseEAR.EDIT_LOCK_TYPE_ID_CONFIG,
-						newObjectforEditLock.get(), 
+						newObjectforEditLock, 
 						"testEditLock-francois", 
 						new String[] {FetchPlan.DEFAULT, 
-					EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
-					EditLock.FETCH_GROUP_EDIT_LOCK_TYPE}, 
-					NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
+						EditLock.FETCH_GROUP_LOCK_OWNER_USER, 
+						EditLock.FETCH_GROUP_EDIT_LOCK_TYPE}, 
+						NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);	
 				if(acquireEditLockResult.getEditLockCount()!= 2)
 					fail("the Object should have been locked already !!!!");				
-				Set<EditLockID> editLockIDs = elm.getEditLockIDs(newObjectforEditLock.get());
+				Set<EditLockID> editLockIDs = elm.getEditLockIDs(newObjectforEditLock);
 				logger.info("our new Object Got EditLocks:" + editLockIDs.size());
 				Collection<EditLock> editLocks = elm.getEditLocks(editLockIDs, 
 						FETCH_GROUP_EDITLOCK, 
@@ -152,13 +147,13 @@ public class EditLockTestCase extends TestCase{
 					logger.info("Locked-Object-ID :" + editLock.getLockedObjectIDStr());
 					logger.info("-------------------------------------------");
 				}
-				elm.releaseEditLock(newObjectforEditLock.get(), ReleaseReason.normal);
+				elm.releaseEditLock(newObjectforEditLock, ReleaseReason.normal);
 			}catch (LoginException e) {
 				fail("couldnt login with another User Account to test the EditLock!!!!");		
 			}
-			login.logout();
-			// REV Alex: try-finally for the login missing. An error inbetween might
-			// have influence on other tests. Detect-test is ok.
+			finally{
+				login.logout();
+			}
 		}
 	
 	private PropertySetID createDemoObjectForEditLocking() throws Exception{	
@@ -176,10 +171,5 @@ public class EditLockTestCase extends TestCase{
 		person.deflate();
 		person = (Person) pm.storePropertySet(person, true, null, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
 		return (PropertySetID)JDOHelper.getObjectId(person);
-	}
-	@Override
-	protected void tearDown()throws Exception
-	{
-		super.tearDown();
 	}
 }
--- src/org/nightlabs/jfire/testsuite/language/LanguageTestCase.java
+++ src/org/nightlabs/jfire/testsuite/language/LanguageTestCase.java
@@ -1,48 +1,111 @@
 package org.nightlabs.jfire.testsuite.language;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.both;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.nightlabs.jfire.testsuite.hamcrest.IsNotEmptyMatcher.isNotEmpty;
+import static org.nightlabs.jfire.testsuite.hamcrest.IsNotNullMatcher.isNotNull;
+
 import java.util.Collection;
 import java.util.Locale;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import javax.jdo.FetchPlan;
+import javax.jdo.JDOHelper;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.nightlabs.jdo.NLJDOHelper;
 import org.nightlabs.jfire.base.JFireEjb3Factory;
 import org.nightlabs.jfire.language.Language;
+import org.nightlabs.jfire.language.LanguageConfig;
 import org.nightlabs.jfire.language.LanguageManagerRemote;
+import org.nightlabs.jfire.language.LanguageSyncMode;
+import org.nightlabs.jfire.language.id.LanguageID;
 import org.nightlabs.jfire.security.SecurityReflector;
 import org.nightlabs.jfire.testsuite.JFireTestSuite;
+import org.nightlabs.jfire.testsuite.TestCase;
 import org.nightlabs.language.LanguageCf;
 
 /**
-*
-* @author Fitas Amine - fitas [at] nightlabs [dot] de
-*
-*/
+ *
+ * a simple test case to test the API of the Language Manager Bean
+ * 
+ * @author Fitas Amine - fitas [at] nightlabs [dot] de
+ *
+ */
 @JFireTestSuite(JFireBaseLanguageTestSuite.class)
 public class LanguageTestCase extends TestCase{
-	
+
 	Logger logger = Logger.getLogger(LanguageTestCase.class);	
+	private static String NEW_LANGUAGE = "NEWLANGUAGE";
+
 
 	@Test
-	public void testLanguage() throws Exception{	
-		LanguageCf newLanguageCf = new LanguageCf("epo");
+	public void testAddNewLanguage() throws Exception{	
+		LanguageManagerRemote  lm = JFireEjb3Factory.getRemoteBean(LanguageManagerRemote.class, SecurityReflector.getInitialContextProperties());	
+		LanguageCf newLanguageCf = new LanguageCf("mak");
 		newLanguageCf.init(null);
-		newLanguageCf.setNativeName("Esperanto");
-		newLanguageCf.getName().setText(Locale.ENGLISH.getLanguage(),"Esperanto");
-		LanguageManagerRemote  lm = JFireEjb3Factory.getRemoteBean(LanguageManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		newLanguageCf.setNativeName("Makasar");
+		newLanguageCf.getName().setText(Locale.ENGLISH.getLanguage(),"Makasar");
 		// creates the new language and if it exists it does nothing
-		Language newLanguage = lm.createLanguage(newLanguageCf, true, false);	
+		Language newLang = lm.createLanguage(newLanguageCf, false, true);	
+		assertThat(newLang,notNullValue());
+		assertThat(newLang.getLanguageID(),equalTo(newLanguageCf.getLanguageID()));
 		Collection<Language> languages = lm.getLanguages();		
+		assertThat(languages,both(isNotNull()).and(isNotEmpty())); 
 		// List all the languages available in the system.
-		logger.info("The following Languages was found on the system.");
-		for (Language lang : languages) {
-			logger.info("Language = "+ lang.getName().getText());
+		boolean found = false;
+		for (Language lang : languages) 
+		{
+			if(lang.getLanguageID().equals(newLang.getLanguageID()))
+				found = true;
 		}
-		// REV Alex: A check is missing whether what the server returned was
-		// what this test expects it to return.
-//		Assert.assertEquals(newLanguageCf.getLanguageID(), newLanguage.getLanguageID());
-		// and more...
+		assertThat(found,equalTo(true));
+		setTestCaseContextObject(NEW_LANGUAGE, (LanguageID)JDOHelper.getObjectId(newLang));
+		// calls the method with get set to false
+		newLang = lm.createLanguage(newLanguageCf, false, false);	
+		assertThat(newLang,nullValue());
+	}	
+
+	@Test
+	public void testLanguageConfig() throws Exception{	
+
+		LanguageManagerRemote  lm = JFireEjb3Factory.getRemoteBean(LanguageManagerRemote.class, SecurityReflector.getInitialContextProperties());
+
+		// Get language manager, configuration and current sync mode.
+		lm = JFireEjb3Factory.getRemoteBean(LanguageManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		LanguageConfig languageConfig = lm.getLanguageConfig(new String[]{FetchPlan.DEFAULT}, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+		assertThat(languageConfig,notNullValue());
+		LanguageSyncMode syncmode = languageConfig.getLanguageSyncMode();
+		languageConfig.setLanguageSyncMode(LanguageSyncMode.off);
+		languageConfig = lm.storeLanguageConfig(languageConfig, 
+				true, 
+				new String[]{FetchPlan.DEFAULT},
+				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+		// switch language sync mode to Off
+		assertThat(languageConfig.getLanguageSyncMode(),equalTo(LanguageSyncMode.off));
+		languageConfig.setLanguageSyncMode(syncmode);	
+		// restores back the original value
+		languageConfig = lm.storeLanguageConfig(languageConfig, 
+				false, 
+				null,
+				NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT);
+		assertThat(languageConfig,nullValue());
+	}
+
+	
+	@Test
+	public void testDeleteLanguage() throws Exception{	
+		LanguageManagerRemote  lm = JFireEjb3Factory.getRemoteBean(LanguageManagerRemote.class, SecurityReflector.getInitialContextProperties());
+		LanguageID newLanguageID = (LanguageID)getTestCaseContextObject(NEW_LANGUAGE);
+		assertThat(newLanguageID,notNullValue()); 
+		// delete the language !!!!			
+		lm.deleteLanguage(newLanguageID);	
+		Collection<Language> languages = lm.getLanguages();		
+		for (Language lang : languages) 
+			assertThat(lang.getLanguageID(),not(equalTo(newLanguageID.languageID)));
 	}
 }
