7f703e616bddad5cbeaacae5ea314c75f891b569
fitas_java <fitas_java@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-05-17 06:28:07 +0000

    moved the test case JFirePropertySetTestCase inside JFireTestSuiteJFireBase Project
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireTestSuiteJFireBase@17695 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/testsuite/base/prop/JFirePropertySetTestSuite.java
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+package org.nightlabs.jfire.testsuite.base.prop;
+
+import javax.jdo.PersistenceManager;
+
+import junit.framework.TestCase;
+
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.testsuite.TestSuite;
+import org.nightlabs.jfire.testsuite.prop.PropertySetTestStruct;
+
+/**
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
+ *
+ */
+public class JFirePropertySetTestSuite extends TestSuite {
+
+	/**
+	 * @param classes
+	 */
+	public JFirePropertySetTestSuite(Class<? extends TestCase>... classes) {
+		super(classes);
+	}
+
+	/** {@inheritDoc}
+	 */
+	@Override
+	public String canRunTests(PersistenceManager pm) throws Exception {
+		PropertySetTestStruct.getTestStruct(SecurityReflector.getUserDescriptor().getOrganisationID(), pm);
+		pm.flush();
+		return null;
+	}
+
+}
