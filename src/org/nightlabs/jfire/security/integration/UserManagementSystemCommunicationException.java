b675512e7591142b294f1aa03c111f13a02021f1
ddudnik <ddudnik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-12-05 16:50:09 +0000

    added comments
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base@18311 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/security/integration/UserManagementSystemCommunicationException.java
+++ src/org/nightlabs/jfire/security/integration/UserManagementSystemCommunicationException.java
@@ -1,21 +1,40 @@
 package org.nightlabs.jfire.security.integration;
 
+/**
+ * This exception is thrown when we can't communicate to UserManagementSystem
+ * due to some connection problems.  
+ * 
+ * @author Denis Dudnik <deniska.dudnik[at]gmail{dot}com>
+ *
+ */
 public class UserManagementSystemCommunicationException extends Exception{
 
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public UserManagementSystemCommunicationException(){
 		super();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public UserManagementSystemCommunicationException(String msg){
 		super(msg);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public UserManagementSystemCommunicationException(String msg, Throwable t){
 		super(msg, t);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public UserManagementSystemCommunicationException(Throwable t){
 		super(t);
 	}
