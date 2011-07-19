20c3292ce93640aa61100be9dc3f8e6c26f2847c
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-07-19 14:07:51 +0000

    Removed old, commented code.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireServerManager@19055 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/organisationinit/OrganisationInitDependency.java
+++ src/org/nightlabs/jfire/organisationinit/OrganisationInitDependency.java
@@ -10,38 +10,27 @@ public class OrganisationInitDependency implements IDependency<OrganisationInit>
 	private static final long serialVersionUID = 1L;
 
 	private String invocation;
-//	private String module;
-//	private String archive;
-//	private String bean;
-//	private String method;
 	private Resolution resolution;
 
-//	public OrganisationInitDependency(String module, String archive, String bean, String method, Resolution res) {
-//		this.module = module;
-//		this.archive = archive;
-//		this.bean = bean;
-//		this.method = method;
-//		this.resolution = res;
-//	}
 	public OrganisationInitDependency(String invocation, Resolution res) {
 		if (invocation == null)
 			throw new IllegalArgumentException("invocation == null");
-		
+
 		this.invocation = invocation;
 		this.resolution = res;
 	}
-	
+
 	public String getInvocation() {
 		return invocation;
 	}
-	
+
 	public void setInvocation(String invocation) {
 		this.invocation = invocation;
 		this.invocationPath = null;
 	}
-	
+
 	private String[] invocationPath;
-	
+
 	public String[] getInvocationPath() {
 		if (invocationPath == null) {
 			this.invocationPath = invocation.split(".");
@@ -49,43 +38,12 @@ public class OrganisationInitDependency implements IDependency<OrganisationInit>
 		return invocationPath;
 	}
 
-//	public String getArchive() {
-//		return archive;
-//	}
-//
-//	public void setArchive(String archive) {
-//		this.archive = archive;
-//	}
-//
-//	public String getBean() {
-//		return bean;
-//	}
-//
-//	public void setBean(String bean) {
-//		this.bean = bean;
-//	}
-//
-//	public String getMethod() {
-//		return method;
-//	}
-//
-//	public void setMethod(String method) {
-//		this.method = method;
-//	}
-//
-//	public String getModule() {
-//		return module;
-//	}
-//
-//	public void setModule(String module) {
-//		this.module = module;
-//	}
-
 	@Override
 	public String toString() {
 		return this.getClass().getSimpleName() + ": " + invocation + " (" + resolution.toString() + ")";
 	}
 
+	@Override
 	public Resolution getResolution() {
 		return resolution;
 	}
