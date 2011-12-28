2830f155fa56a3392985fe8bbd262ac982858909
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-12-28 18:00:12 +0000

    * started to implement config page actions
    git-svn-id: https://svn.jfire.org/svn/main/trunk/JFireDashboard@20180 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/dashboard/DashboardGadgetClientScriptsConfig.java
+++ src/org/nightlabs/jfire/dashboard/DashboardGadgetClientScriptsConfig.java
@@ -13,13 +13,18 @@ public class DashboardGadgetClientScriptsConfig implements Serializable {
 	private boolean confirmProcessing;
 	
 	public DashboardGadgetClientScriptsConfig() { 
-		clientScripts = new ArrayList<ClientScript>();
+		clientScripts = new ArrayList<DashboardGadgetClientScriptsConfig.ClientScript>();
 	}
 
 	public static class ClientScript {
 		
 		private String name;
 		private String script;
+		
+		public ClientScript() {
+			this.name = "";
+			this.script = "";
+		}
 		
 		public ClientScript(final String name, final String script) {
 			this.name = name;
@@ -34,12 +39,16 @@ public class DashboardGadgetClientScriptsConfig implements Serializable {
 			return script;
 		}
 	}
+	
+	public ClientScript createNewClientScript(final String name, final String script) {
+		return new ClientScript(name, script);
+	}
 
-	public List<ClientScript> getClientScripts() {
+	public List<DashboardGadgetClientScriptsConfig.ClientScript> getClientScripts() {
 		return clientScripts;
 	}
 
-	public void setClientScripts(final List<ClientScript> clientScripts) {
+	public void setClientScripts(final List<DashboardGadgetClientScriptsConfig.ClientScript> clientScripts) {
 		this.clientScripts = clientScripts;
 	}
 
