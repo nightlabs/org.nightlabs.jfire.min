aa28e8a8b3e63823c10b1b2360bb33f43ab2f87a
bieber <bieber@nightlabs.de>
2013-11-23 18:30:02 +0100

    remove wrong top-level things

--- src/org/nightlabs/jfire/asyncinvoke/DelegateL.java
+++
@@ -1,7 +0,0 @@
-package org.nightlabs.jfire.asyncinvoke;
-
-import javax.ejb.EJBLocalObject;
-
-public interface DelegateL extends Delegate, EJBLocalObject
-{
-}
--- src/org/nightlabs/jfire/organisation/RoleConstants.java
+++
@@ -1,8 +0,0 @@
-package org.nightlabs.jfire.organisation;
-
-import org.nightlabs.jfire.security.id.RoleID;
-
-public class RoleConstants {
-	public static final RoleID queryOrganisations = RoleID.create("org.nightlabs.jfire.organisation.queryOrganisations");
-	public static final RoleID manageCrossOrganisationRegistrations = RoleID.create("org.nightlabs.jfire.organisation.manageCrossOrganisationRegistrations");
-}
